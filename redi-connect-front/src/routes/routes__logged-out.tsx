b5ad45084217ab2337d8c79ecb17fe60bf3f0c7e
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-02 14:12:58 +0200

    dont lazy load the initial/default login route

--- redi-connect-front/src/routes/routes__logged-out.tsx
+++ redi-connect-front/src/routes/routes__logged-out.tsx
@@ -1,9 +1,10 @@
 import { RouteDefinition } from './index';
 import { lazy } from 'react'
+import Login from '../pages/front/login/Login'
 const SignUpFormMentee= lazy(() => import(/* webpackChunkName: "SignUpFormMentee", webpackPreload: true */ '../pages/front/signup/SignUpFormMentee'));
 const SignUpFormMentor= lazy(() => import(/* webpackChunkName: "SignUpFormMentor", webpackPreload: true */ '../pages/front/signup/SignUpFormMentor'));
 const SignUpComplete= lazy(() => import(/* webpackChunkName: "SignUpComplete", webpackPreload: true */ '../pages/front/signup/SignUpComplete'));
-const Login= lazy(() => import(/* webpackChunkName: "Login", webpackPreload: true */ '../pages/front/login/Login'));
+// const Login= lazy(() => import(/* webpackChunkName: "Login", webpackPreload: true */ '../pages/front/login/Login'));
 const SignUpExisting= lazy(() => import(/* webpackChunkName: "SignUpExisting", webpackPreload: true */ '../pages/front/signup-existing/SignUpExisting'));
 const ResetPassword= lazy(() => import(/* webpackChunkName: "ResetPassword", webpackPreload: true */ '../pages/front/signup-existing/ResetPassword'));
 
