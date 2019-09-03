18478751ef93da8c0bf471e451cbff53f7bc48c4
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-03 14:49:52 +0200

    chore: comment out whyDidYouRender for now to avoid spamming the console

--- redi-connect-front/src/index.tsx
+++ redi-connect-front/src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker'
 import App from './App';
 // Needed for datepicker in <LogMentoringSessionDialog>
 import 'date-fns'
 
-if (process.env.NODE_ENV !== 'production') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    include: [/.*/]
-  });
-}
+// uncomment this to see wasted/unnecessary renders of your components
+// if (process.env.NODE_ENV !== 'production') {
+  // const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  // whyDidYouRender(React, {include: [/.*/]});
+// }
 
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
-
-
-serviceWorker.register()
