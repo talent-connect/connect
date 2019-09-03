7f24540b39c588fd278f5914a479b27f5720db69
Khaled Osman <khaled.osmaan@gmail.com>
2019-09-03 14:38:16 +0200

    chore: add why-did-you-render to notify about wasted renders + use strictMode

--- redi-connect-front/src/index.tsx
+++ redi-connect-front/src/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorker from './serviceWorker'
 import App from './App';
 // Needed for datepicker in <LogMentoringSessionDialog>
 import 'date-fns'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV !== 'production') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    include: [/.*/]
+  });
+}
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+
+
+serviceWorker.register()
