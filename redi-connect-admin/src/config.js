b51e8da145a0d2f796aab4735a6a1da59d19040f
Eric Bolikowski <eric@binarylights.com>
2019-09-13 18:33:31 +0200

    feat: parameterize env for new 'demo' env.

--- redi-connect-admin/src/config.js
+++ redi-connect-admin/src/config.js
@@ -1,4 +1,16 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://connect-api.redi-school.org/api'
-    : 'http://127.0.0.1:3003/api';
+function buildApiUrl(env) {
+  switch (env) {
+    case 'production':
+      return 'https://connect-api.redi-school.org/api';
+
+    case 'demonstration':
+      return 'https://api.demo.connect.redi-school.org/api';
+
+    default:
+    case 'development':
+    case 'dev':
+      return 'http://127.0.0.1:3003/api';
+  }
+}
+
+export const API_URL = buildApiUrl(process.env.NODE_ENV);
--- redi-connect-backend/server/ssl-config.js
+++ redi-connect-backend/server/ssl-config.js
@@ -2,11 +2,26 @@ var path = require('path'),
   fs = require('fs');
 
 if (!process.env.HTTP) {
+  let environmentFolder;
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      environmentFolder = 'production';
+      break;
+
+    case 'demonstration':
+    default:
+      environmentFolder = 'demonstration';
+      break;
+  }
   exports.privateKey = fs
-    .readFileSync(path.join(__dirname, './ssl/privkey.pem'))
+    .readFileSync(
+      path.join(__dirname, './' + environmentFolder + 'ssl/privkey.pem')
+    )
     .toString();
   exports.certificate = fs
-    .readFileSync(path.join(__dirname, './ssl/cert.pem'))
+    .readFileSync(
+      path.join(__dirname, './' + environmentFolder + 'ssl/cert.pem')
+    )
     .toString();
 } else {
   exports = {};
