bfd12457e055201d7e394fd585beebd53e5d1eac
Eric Bolikowski <eric@binarylights.com>
2019-09-14 12:01:12 +0200

    chore: change env. variable injection to use env-cmd

--- redi-connect-admin/src/config.js
+++ redi-connect-admin/src/config.js
@@ -1,16 +1,3 @@
-function buildApiUrl(env) {
-  switch (env) {
-    case 'production':
-      return 'https://connect-api.redi-school.org/api';
-
-    case 'demonstration':
-      return 'https://api.demo.connect.redi-school.org/api';
-
-    default:
-    case 'development':
-    case 'dev':
-      return 'http://127.0.0.1:3003/api';
-  }
-}
-
-export const API_URL = buildApiUrl(process.env.NODE_ENV);
+export const API_URL = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : 'http://127.0.0.1:3003/api';
--- redi-connect-front/src/config/config.tsx
+++ redi-connect-front/src/config/config.tsx
@@ -147,4 +147,6 @@ function buildApiUrl(env: string) {
   }
 }
 
-export const API_URL = buildApiUrl(process.env.NODE_ENV);
+export const API_URL = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : 'http://127.0.0.1:3003/api';
