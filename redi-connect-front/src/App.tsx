87563a9e54e945757429194364e3324d52bcac55
Eric Bolikowski <eric@binarylights.com>
2019-10-10 11:22:01 +0200

    feat(front): add snackbar notification mechanism (inspired by Khaled)

--- redi-connect-front/src/App.tsx
+++ redi-connect-front/src/App.tsx
@@ -7,13 +7,12 @@ import { history, Router } from './services/history/history';
 import { Routes } from './components/Routes';
 import { store } from './redux/store';
 import { profileFetchStart } from './redux/user/actions';
-
-const mainColour = '#58adc4';
+import AppNotification from './components/AppNotification';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: mainColour,
+      main: '#58adc4',
       contrastText: '#fff',
     },
     secondary: {
@@ -54,10 +53,11 @@ const theme = createMuiTheme({
 
 const App = () => {
   useEffect(() => {
-    store.dispatch(profileFetchStart())
-  }, [])
+    store.dispatch(profileFetchStart());
+  }, []);
   return (
     <MuiThemeProvider theme={theme}>
+      <AppNotification />
       <StoreProvider store={store}>
         <Router history={history}>
           <Suspense fallback={<h3>Loading...</h3>}>
