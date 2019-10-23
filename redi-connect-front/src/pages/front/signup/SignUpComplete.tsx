91687a1009f2ce29e6d986390f285ff8108abb28
Eric Bolikowski <eric@binarylights.com>
2019-10-23 17:10:50 +0200

    chore(front): further text update

--- redi-connect-front/src/pages/front/signup/SignUpComplete.tsx
+++ redi-connect-front/src/pages/front/signup/SignUpComplete.tsx
@@ -15,17 +15,30 @@ export default function SignUpComplete({
   // const Comp: any = (props: any) => <Link {...props} to="/app/dashboard" />;
   return (
     <LoggedOutLayout>
-      <p>
-        Thanks for registering! We're thrilled that you're ReDI :) But there is
-        one last step before we can activate your profile because we want to
-        make sure you find the right mentor:
-      </p>
-      <p>
-        Simply contact Isabelle via e-mail isabelle@redi-school.org to schedule
-        a meeting. Or just turn up anytime between 5 and 8 on a Tuesday at ReDI
-        School.
-      </p>
-      <p>You can also find and write to Isabelle on the ReDI Slack.</p>
+      {type === 'public-sign-up-mentee-pending-review' && (
+        <>
+          <p>
+            Thanks for registering! We're thrilled that you're ReDI :) But there
+            is one last step before we can activate your profile because we want
+            to make sure you find the right mentor:
+          </p>
+          <p>
+            Simply contact Isabelle via e-mail isabelle@redi-school.org to
+            schedule a meeting. Or just turn up anytime between 5 and 8 on a
+            Tuesday at ReDI School.
+          </p>
+          <p>You can also find and write to Isabelle on the ReDI Slack.</p>
+        </>
+      )}
+      {type === 'public-sign-up-mentor-pending-review' && (
+        <>
+          <p>
+            Thanks for registering! We're thrilled that you're ReDI :) We
+            promise to review your profile as quickly as possible. We'll send
+            you an email once we're done.
+          </p>
+        </>
+      )}
     </LoggedOutLayout>
   );
 }
