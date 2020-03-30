f46817d9f6e39d184f40b848e0ce279d0d209160
Endre Zónai <ezonai@deloitte.de>
2020-03-30 08:07:47 +0200

    feat(front): login page UI

--- redi-connect-front/src/layouts/LoggedOutLayout.tsx
+++ redi-connect-front/src/layouts/LoggedOutLayout.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
 
-import AppBar from '@material-ui/core/AppBar';
-import { CssBaseline, Button, Toolbar } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-
-import rediLogo from '../assets/rediLogo.svg';
-
 interface Props {
   children: React.ReactNode;
+  modifier?: String;
 }
 
-export const LoggedOutLayout = ({ children }: Props) => (
-  <>
-    <CssBaseline />
-    <AppBar position="static">
-      <Toolbar>
-        <Button
-          style={{ margin: 0, padding: 0 }}
-          component={props => <Link {...props} to="/front/home" />}
-        >
-          <img
-            src={rediLogo}
-            style={{ height: '36px', width: '96px' }}
-            alt="redi logo"
-          />
-        </Button>
-      </Toolbar>
-    </AppBar>
-    <div style={{ margin: '12px' }}>{children}</div>
-  </>
+export const LoggedOutLayout = ({ children, modifier }: Props) => (
+  // this will be done in a more elegant way when the layout wrapper is done
+  <div className={`page ${modifier}`}>{children}</div>
 );
--- redi-connect-front/src/pages/front/login/Login.tsx
+++ redi-connect-front/src/pages/front/login/Login.tsx
@@ -1,41 +1,18 @@
-import React, { useState } from "react";
-import { LoggedOutLayout } from "../../../layouts/LoggedOutLayout";
-import * as Yup from "yup";
-import {
-  Container,
-  TextField,
-  InputAdornment,
-  Link,
-  Button,
-  Paper,
-  Theme,
-  createStyles,
-  withStyles,
-  Typography,
-  Box,
-} from "@material-ui/core";
-import { Link as RouterLink } from "react-router-dom";
-import { Lock as LockIcon, Person as PersonIcon } from "@material-ui/icons";
-import { Formik, FormikProps, FormikActions, FormikValues } from "formik";
-import { history } from "../../../services/history/history";
-import { login, fetchSaveRedProfile } from "../../../services/api/api";
-import { saveAccessToken } from "../../../services/auth/auth";
+import React, { useState } from 'react';
+import { LoggedOutLayout } from '../../../layouts/LoggedOutLayout';
+import * as Yup from 'yup';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Formik, FormikActions, FormikValues } from 'formik';
+import { history } from '../../../services/history/history';
+import { login, fetchSaveRedProfile } from '../../../services/api/api';
+import { saveAccessToken } from '../../../services/auth/auth';
 
-const styles = (theme: Theme) =>
-  createStyles({
-    loginError: {
-      padding: theme.spacing(1),
-      backgroundColor: theme.palette.error.main,
-      color: "white",
-    },
-    headingMargin: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(5),
-    },
-    navigationBox: {
-      marginTop: theme.spacing(4),
-    },
-  });
+import { ReactComponent as WelcomeIllustration } from '../../../assets/welcome-2.svg';
+
+import './Login.scss';
+import { Columns, Form, Heading, Content } from 'react-bulma-components';
+// import { Heading } from './Heading';
 
 interface LoginFormValues {
   username: string;
@@ -43,24 +20,26 @@ interface LoginFormValues {
 }
 
 const initialValues: LoginFormValues = {
-  username: "",
-  password: "",
+  username: '',
+  password: '',
 };
+
+const BASE_CLASS = 'login-form';
 
 const validationSchema = Yup.object({
   username: Yup.string()
     .email()
     .required()
-    .label("Email")
+    .label('Email')
     .max(255),
   password: Yup.string()
     .required()
-    .label("Password")
+    .label('Password')
     .max(255),
 });
 
 export default function Login() {
-  const [loginError, setLoginError] = useState<string>("");
+  const [loginError, setLoginError] = useState<string>('');
 
   const submitForm = async (
     values: FormikValues,
@@ -70,144 +49,142 @@ export default function Login() {
     try {
       const accessToken = await login(formValues.username, formValues.password);
       saveAccessToken(accessToken);
-      const myProfile = await fetchSaveRedProfile(accessToken);
-      history.push("/app/dashboard");
+      await fetchSaveRedProfile(accessToken);
+      history.push('/app/dashboard');
     } catch (err) {
-      setLoginError("Invalid username or password");
+      setLoginError('You entered an incorrect email, password, or both.');
     }
     actions.setSubmitting(false);
   };
 
+  const change = (
+    name: any,
+    handleChange: any,
+    setFieldTouched: any,
+    e: any
+  ) => {
+    e.persist();
+    handleChange(e);
+    setFieldTouched(name, true, false);
+  };
+
   return (
-    <LoggedOutLayout>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={submitForm}
-        render={props => <Form {...props} loginError={loginError} />}
-      />
+    <LoggedOutLayout modifier="page--color-half">
+      <Columns vCentered>
+        <Columns.Column
+          size={6}
+          responsive={{ mobile: { hide: { value: true } } }}
+        >
+          <WelcomeIllustration className="welcome-2" />
+          <Content textTransform="uppercase" size="5">
+            Don't have an account yet?{' '}
+            <Link to="/front/signup/landing">signup here</Link>
+          </Content>
+        </Columns.Column>
+
+        <Columns.Column size={4} offset="1">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={submitForm}
+            render={props => (
+              <>
+                <Heading
+                  size="1"
+                  weight="medium"
+                  renderAs="h1"
+                  className="title--border"
+                >
+                  Sign-in
+                </Heading>
+                <Heading size="4" renderAs="p" subtitle>
+                  Enter your email and password below.
+                </Heading>
+                <form onSubmit={e => e.preventDefault()} className="login-form">
+                  <Form.Field>
+                    <Form.Control>
+                      <Form.Input
+                        id="username"
+                        name="username"
+                        type="email"
+                        color={
+                          props.touched.username &&
+                          Boolean(props.errors.username)
+                            ? 'danger'
+                            : ''
+                        }
+                        placeholder="Email"
+                        value={props.values.username}
+                        onChange={change.bind(
+                          null,
+                          'username',
+                          props.handleChange,
+                          props.setFieldTouched
+                        )}
+                        disabled={props.isSubmitting}
+                      />
+                    </Form.Control>
+                    {props.touched.username &&
+                      Boolean(props.errors.username) && (
+                        <Form.Help color="danger">
+                          This email is invalid
+                        </Form.Help>
+                      )}
+                  </Form.Field>
+
+                  <Form.Field>
+                    <Form.Control>
+                      <Form.Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        color={
+                          props.touched.password &&
+                          Boolean(props.errors.password)
+                            ? 'danger'
+                            : ''
+                        }
+                        placeholder="Password*"
+                        value={props.values.password}
+                        onChange={change.bind(
+                          null,
+                          'password',
+                          props.handleChange,
+                          props.setFieldTouched
+                        )}
+                        disabled={props.isSubmitting}
+                      />
+                    </Form.Control>
+                    {loginError && (
+                      <Form.Help color="danger">{loginError}</Form.Help>
+                    )}
+                  </Form.Field>
+                  <Form.Field
+                    className={`${BASE_CLASS}__reset-password`}
+                    textTransform="uppercase"
+                  >
+                    <Link to="/front/reset-password/request-reset-password-email">
+                      Forgot your password?
+                    </Link>
+                  </Form.Field>
+                  <Form.Field className={`${BASE_CLASS}__submit`}>
+                    <Button
+                      onClick={props.submitForm}
+                      type="submit"
+                      color="primary"
+                      variant="contained"
+                      fullWidth
+                      disabled={props.isSubmitting || !props.isValid}
+                    >
+                      Log in
+                    </Button>
+                  </Form.Field>
+                </form>
+              </>
+            )}
+          />
+        </Columns.Column>
+      </Columns>
     </LoggedOutLayout>
   );
 }
-
-const Form = withStyles(styles)(
-  (
-    props: FormikProps<LoginFormValues> & {
-      loginError: string;
-      classes: {
-        loginError: string;
-        headingMargin: string;
-        navigationBox: string;
-      };
-    }
-  ) => {
-    const {
-      classes,
-      loginError,
-      values: { username, password },
-      errors,
-      touched,
-      handleChange,
-      isValid,
-      isSubmitting,
-      setFieldTouched,
-      submitForm,
-    } = props;
-
-    const change = (name: any, e: any) => {
-      e.persist();
-      handleChange(e);
-      setFieldTouched(name, true, false);
-    };
-
-    return (
-      <Container maxWidth="sm">
-        <Typography
-          variant="h5"
-          align="center"
-          className={classes.headingMargin}
-        >
-          Login to ReDI Connect
-        </Typography>
-        {loginError && (
-          <Paper className={classes.loginError}>{loginError}</Paper>
-        )}
-        <form onSubmit={e => e.preventDefault()}>
-          <TextField
-            id="username"
-            name="username"
-            type="email"
-            helperText={touched.username ? errors.username : ""}
-            error={touched.username && Boolean(errors.username)}
-            label="Username (your email address)*"
-            value={username}
-            onChange={change.bind(null, "username")}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PersonIcon />
-                </InputAdornment>
-              ),
-            }}
-            fullWidth
-            margin="normal"
-            disabled={isSubmitting}
-          />
-          <TextField
-            id="password"
-            name="password"
-            type="password"
-            helperText={touched.password ? errors.password : ""}
-            error={touched.password && Boolean(errors.password)}
-            label="Password*"
-            value={password}
-            onChange={change.bind(null, "password")}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockIcon />
-                </InputAdornment>
-              ),
-            }}
-            fullWidth
-            margin="normal"
-            disabled={isSubmitting}
-          />
-          <Button
-            onClick={submitForm}
-            type="submit"
-            color="primary"
-            variant="contained"
-            fullWidth
-            disabled={isSubmitting || !isValid}
-          >
-            Log in
-          </Button>
-          <Box className={classes.navigationBox}>
-            <Typography align="center">
-              <Link
-                component={RouterLink}
-                to="/front/reset-password/request-reset-password-email"
-              >
-                Forgot your password?
-              </Link>
-            </Typography>
-          </Box>
-          <Box className={classes.navigationBox}>
-            <Typography align="center">
-              New to ReDI Connect? Join as a{" "}
-              <Link component={RouterLink} to="/app/create-profile/mentee">
-                student
-              </Link>{" "}
-              or{" "}
-              <Link component={RouterLink} to="/app/create-profile/mentor">
-                mentor
-              </Link>
-              .
-            </Typography>
-          </Box>
-        </form>
-      </Container>
-    );
-  }
-);
