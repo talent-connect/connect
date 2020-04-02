94b750bd43d926c02c6834a4d13a7f18feee4fcf
Marcus Zierke <mzierke@deloitte.de>
2020-04-02 17:20:15 +0200

    build(front): preFooter component added

--- redi-connect-front/src/pages/front/landing/Home.tsx
+++ redi-connect-front/src/pages/front/landing/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LoggedOutWrapper from "../../../components/organisms/LoggedOutWrapper";
 import Hero from "../../../components/organisms/RediHero";
+import PreFooter from "../../../components/organisms/PreFooter";
 
 export default function Home() {
   return (
     <LoggedOutWrapper>
       <Hero />
+      <PreFooter />
     </LoggedOutWrapper>
   );
 }
