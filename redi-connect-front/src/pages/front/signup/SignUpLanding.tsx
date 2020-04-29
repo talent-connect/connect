40adc74327886dfd3c99ea1a78b799dd07734886
Marcus Zierke <mzierke@deloitte.de>
2020-04-29 11:21:13 +0200

    build(front): signup landing page added

--- redi-connect-front/src/pages/front/signup/SignUpLanding.tsx
+++ redi-connect-front/src/pages/front/signup/SignUpLanding.tsx
@@ -1,38 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Columns } from 'react-bulma-components'
-import { LoggedOutLayout } from '../../../layouts/LoggedOutLayout'
+import React, { useState, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Image, Heading, Columns } from 'react-bulma-components'
+import AccountOperation from '../../../components/templates/AccountOperation'
+import Teaser from '../../../components/molecules/Teaser'
+import Button from '../../../components/atoms/Button'
+import mentee from '../../../assets/images/mentee.svg'
+import mentor from '../../../assets/images/mentor.svg'
+import './SignUpLanding.scss'
 
-export default function SignUpLanding () {
+interface Props {
+  history: any
+}
+
+const SignUpLanding = ({ history }: Props) => {
+  const [selectedRole, setSelectedRole] = useState('')
+  const [isDisabled, setIsDisabled] = useState(true)
+  const [mentorClass, setMentorClass] = useState('')
+  const [menteeClass, setMenteeClass] = useState('')
+
+  useEffect(() => {
+    if (selectedRole !== '') {
+      setIsDisabled(false)
+      setMenteeClass(selectedRole === 'mentee' ? 'border-mentee' : 'no-shadow')
+      setMentorClass(selectedRole === 'mentor' ? 'border-mentor' : 'no-shadow')
+    }
+  }, [selectedRole])
+
   return (
-    <LoggedOutLayout>
-      <Columns>
-        <Columns.Column size={6}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-          <Link to="/front/signup/mentee">mentee</Link>
+    <AccountOperation>
+      <Columns vCentered>
+        <Columns.Column
+          size={6}
+          responsive={{ mobile: { hide: { value: true } } }}
+        >
+          <Teaser.SignIn />
         </Columns.Column>
 
-        <Columns.Column size={6}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-          <Link to="/front/signup/mentor">mentor</Link>
+        <Columns.Column size={5} offset={1}>
+          <Heading
+            size={1}
+            weight="normal"
+            renderAs="h1"
+            responsive={{ mobile: { textSize: { value: 2 } } }}
+            className="title--border"
+            spaced={true}
+          >
+            Sign-up
+          </Heading>
+          <Heading size={4} renderAs="p" subtitle>
+            Do you want to become a <strong>mentor</strong> or a <strong>mentee</strong>?
+          </Heading>
+          <div className="signup">
+            <div className={`signup__type ${menteeClass}`} onClick={() => setSelectedRole('mentee')}>
+              <Image src={mentee} alt="mentee" />
+              <Heading className="signup__type--name" renderAs="p">Mentee</Heading>
+            </div>
+            <div className={`signup__type ${mentorClass}`} onClick={() => setSelectedRole('mentor')}>
+              <Image src={mentor} alt="mentor" />
+              <Heading className="signup__type--name" renderAs="p">Mentor</Heading>
+            </div>
+          </div>
+          <Button
+            fullWidth
+            onClick={() => history.push(`/front/signup/${selectedRole}`)}
+            disabled={isDisabled}
+            size="medium"
+          >
+            next step
+          </Button>
         </Columns.Column>
       </Columns>
-    </LoggedOutLayout>
+    </AccountOperation>
   )
 }
+
+export default withRouter(SignUpLanding)
