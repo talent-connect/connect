707521c9cfd8d9417dcd96367dfc6f56611302ea
Marcus Zierke <mzierke@deloitte.de>
2020-05-07 13:15:51 +0200

    fix(front): images in hero, prefooter and navbar mobile menu replaced

--- redi-connect-front/src/components/organisms/RediHero.tsx
+++ redi-connect-front/src/components/organisms/RediHero.tsx
@@ -8,8 +8,7 @@ import {
 } from 'react-bulma-components'
 import { useHistory } from 'react-router-dom'
 import Button from '../atoms/Button'
-import team from '../../assets/images/hero.svg'
-import teamMobile from '../../assets/images/hero-mobile.svg'
+import team from '../../assets/images/hero-new.svg'
 import './RediHero.scss'
 
 const RediHero = () => {
@@ -23,7 +22,7 @@ const RediHero = () => {
             Welcome to ReDI Connect
             </Heading>
             <Columns.Column className="is-hidden-tablet">
-              <img src={teamMobile} alt="team" className="hero-column-img" />
+              <img src={team} alt="team" className="hero-column-img" />
             </Columns.Column>
             <Content className="hero-column-content">
             Are you ready for the future of work?<br />We connect thriving
