0a9eeb75f8844d918d5a30ad6dffc5137c0664ed
Eric Bolikowski <eric@binarylights.com>
2020-05-10 18:44:57 +0200

    chore(front): adjust sign-up-complete text for location = munich

--- redi-connect-front/src/pages/front/signup/SignUpComplete.tsx
+++ redi-connect-front/src/pages/front/signup/SignUpComplete.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { LoggedOutLayout } from '../../../layouts/LoggedOutLayout';
 import { RouteComponentProps } from 'react-router';
+import { RediLocation } from '../../../types/RediLocation';
 
 interface RouteParams {
   type: string;
@@ -15,7 +16,7 @@ export default function SignUpComplete({
   // const Comp: any = (props: any) => <Link {...props} to="/app/dashboard" />;
   return (
     <LoggedOutLayout>
-      {type === 'public-sign-up-mentee-pending-review' && (
+      {type === 'public-sign-up-mentee-pending-review' && (process.env.REACT_APP_REDI_LOCATION as RediLocation) === 'berlin' && (
         <>
           <p>
             Thanks for registering! We're thrilled that you're ReDI :) But there
@@ -30,15 +31,27 @@ export default function SignUpComplete({
           <p>You can also find and write to Isabelle on the ReDI Slack.</p>
         </>
       )}
-      {type === 'public-sign-up-mentor-pending-review' && (
+      {type === 'public-sign-up-mentee-pending-review' && (process.env.REACT_APP_REDI_LOCATION as RediLocation) === 'munich' && (
         <>
           <p>
-            Thanks for registering! We're thrilled that you're ReDI :) We
-            promise to review your profile as quickly as possible. We'll send
-            you an email once we're done.
+            We will review your profile and get in touch with you if there is anything missing. Then, we will approve your profile and you can start searching for mentors.
+          </p>
+          <p>
+            If you have any questions or comments, imply contact Christa via e-mail at <a href="mailto:christa@redi-school.org">christa@redi-school.org</a> or slack: Christa Baron
           </p>
         </>
       )}
-    </LoggedOutLayout>
+      {
+        type === 'public-sign-up-mentor-pending-review' && (
+          <>
+            <p>
+              Thanks for registering! We're thrilled that you're ReDI :) We
+              promise to review your profile as quickly as possible. We'll send
+              you an email once we're done.
+          </p>
+          </>
+        )
+      }
+    </LoggedOutLayout >
   );
 }
