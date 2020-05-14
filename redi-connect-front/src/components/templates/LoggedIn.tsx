74c0b5f511306c8264085ed20c6586d87dd5b5f5
Endre Zónai <ezonai@deloitte.de>
2020-05-14 20:23:18 +0200

    build(front): logged in sidemenu

--- redi-connect-front/src/components/templates/LoggedIn.tsx
+++ redi-connect-front/src/components/templates/LoggedIn.tsx
@@ -3,9 +3,23 @@ import Navbar from '../organisms/Navbar'
 import Button from '../atoms/Button'
 import { Container, Section, Columns } from 'react-bulma-components'
 import { logout } from '../../services/api/api'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import './LoggedIn.scss'
 
 import Footer from '../organisms/Footer'
+
+const MenuItem = ({ url, children }: {url: string, children: string}) => {
+  return (
+    <li className="side-menu__item">
+      <NavLink
+        to={url}
+        className="side-menu__item__link"
+        activeClassName="side-menu__item__link--active">
+        {children}
+      </NavLink>
+    </li>
+  )
+}
 
 const LoggedIn: React.FunctionComponent = ({ children }) => {
   return (
@@ -25,9 +39,11 @@ const LoggedIn: React.FunctionComponent = ({ children }) => {
               size={2}
               responsive={{ mobile: { hide: { value: true } } }}
             >
-              <Link to="/app/me">My Profile</Link> <br/>
-              <Link to="/app/dashboard">Find a mentor</Link><br/>
-              <Link to="/app/applications">My applications</Link>
+              <ul className="side-menu">
+                <MenuItem url="/app/me">My Profile</MenuItem>
+                <MenuItem url="/app/dashboard">Find a mentor</MenuItem>
+                <MenuItem url="/app/applications">Applications</MenuItem>
+              </ul>
             </Columns.Column>
             <Columns.Column
               offset={1}
