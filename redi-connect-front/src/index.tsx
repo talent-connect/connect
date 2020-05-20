5a51f1cc8cac7c7494ef6304687dc84f00b7330f
Eric Bolikowski <eric@binarylights.com>
2020-05-20 10:47:50 +0200

    fix: forgot to take down the 'in maintenance message'

--- redi-connect-front/src/index.tsx
+++ redi-connect-front/src/index.tsx
@@ -10,17 +10,6 @@ import App from './App';
 // whyDidYouRender(React, {include: [/.*/]});
 // }
 
-const DownForMaintenance = () => (
-  <div>
-    <h3>ReDI Connect down a few hours for bug fixes</h3>
-    <p>We've received bug reports from some of you, so we've taken this site down for a few hours while we fix the issues.</p>
-    <p>This won't take too much time, so please check in again tomorrow. We'll get the site up and running as soon as we've fixed the issue.</p>
-  </div>);
-
-if (process.env.REACT_APP_REDI_LOCATION === 'munich' && process.env.NODE_ENV !== "development") {
-  ReactDOM.render(<DownForMaintenance />, document.getElementById('root'));
-} else {
-  ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
-}
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
 
 console.log("This ReDI Connect build is configured for location: ", process.env.REACT_APP_REDI_LOCATION)
