8fcec5877c4e9ea028cf14b107ff72401db2eaf2
Marcus Zierke <mzierke@deloitte.de>
2020-06-10 11:55:06 +0200

    fix(build): columns deleted + minor css updates

--- redi-connect-front/src/components/organisms/Checklist.tsx
+++ redi-connect-front/src/components/organisms/Checklist.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Section, Container, Element, Columns } from 'react-bulma-components'
+import { Section, Container, Element } from 'react-bulma-components'
 import { useTranslation } from 'react-i18next'
 import Heading from '../atoms/Heading'
 import './Checklist.scss'
@@ -31,21 +31,16 @@ const Checklist = ({ type }: Props) => {
         >
           {t(`loggedOutArea.homePage.checklist.${type}.headline`)}
         </Heading>
-        <Columns>
-          {[1, 2, 3, 4, 5].map((number) => (
-            <Columns.Column
-              narrow
-              key={number}
-              desktop={{ offset: 3 }}
-              tablet={{ offset: 1 }}
-              className="checklist__icon content"
-            >
-              <Element textSize={4} responsive={{ mobile: { textSize: { value: 5 } } }}>
-                {t(`loggedOutArea.homePage.checklist.${type}.point${number}`)}
-              </Element>
-            </Columns.Column>
-          ))}
-        </Columns>
+        {[1, 2, 3, 4, 5].map((number) => (
+          <Element
+            key={number}
+            textSize={4}
+            className="checklist__icon"
+            responsive={{ mobile: { textSize: { value: 5 } } }}
+          >
+            {t(`loggedOutArea.homePage.checklist.${type}.point${number}`)}
+          </Element>
+        ))}
       </Container>
     </Section >
   );
