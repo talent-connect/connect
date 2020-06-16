e945d3941404bb663fb84682e005c3c809415ead
Endre Zónai <ezonai@deloitte.de>
2020-06-16 11:07:57 +0200

    fix(front): fix me page loading state

--- redi-connect-front/src/pages/app/me/Me.tsx
+++ redi-connect-front/src/pages/app/me/Me.tsx
@@ -27,91 +27,88 @@ const Me = ({ loading, saveResult, profileFetchStart, profile }: any) => {
     profileFetchStart()
   }, [profileFetchStart])
 
+  if (loading) return <>'page loader placeholder...'</>
+
   const userIsMentee =
     profile.userType === 'mentee' || profile.userType === 'public-sign-up-mentee-pending-review'
 
   return (
     <LoggedIn>
-      {loading && 'page loading...'}
       {saveResult === 'error' && <><br /><br /><br />An error occurred, please try again.</>}
-      {!loading &&
-        <>
-          {saveResult === 'submitting' && 'part of the page loading...'}
+      {saveResult === 'submitting' && 'part of the page loading...'}
 
-          <Columns vCentered breakpoint="mobile" className="oneandhalf-bs">
-            <Columns.Column size={3}>
-              <Avatar.Editable />
+      <Columns vCentered breakpoint="mobile" className="oneandhalf-bs">
+        <Columns.Column size={3}>
+          <Avatar.Editable />
+        </Columns.Column>
+        <Columns.Column size={8}>
+          <Heading>Hi, {profile.firstName}</Heading>
+          <Content size="medium" renderAs="p" responsive={{ mobile: { hide: { value: true } } }}>
+            {`Please fill out your profile. Let potential ${userIsMentee ? 'mentors' : 'mentees'} know a little bit more about you, so you can find the perfect fit. If you have filled out your profile: Great! Make sure you keep it up to date.`}
+          </Content>
+        </Columns.Column>
+      </Columns>
+      <Element className="block-separator" responsive={{ tablet: { hide: { value: true } } }}>
+        <Content size="medium" renderAs="p">
+          {`Please fill out your profile. Let potential ${userIsMentee ? 'mentors' : 'mentees'} know a little bit more about you, so you can find the perfect fit. If you have filled out your profile: Great! Make sure you keep it up to date.`}
+        </Content>
+      </Element>
+      <Element className="block-separator">
+        <EditableAbout />
+      </Element>
+
+      <Element className="block-separator">
+        <EditableMentoringTopics />
+      </Element>
+
+      <Element className="block-separator">
+        <Columns>
+          <Columns.Column size={6}>
+            <Element className="block-separator">
+              <EditableContactDetails />
+            </Element>
+          </Columns.Column>
+          <Columns.Column size={6}>
+            <EditableSocialMedia />
+          </Columns.Column>
+        </Columns>
+      </Element>
+
+      <Element className="block-separator">
+        <Columns>
+          <Columns.Column size={6}>
+            <Element className="block-separator">
+              <EditablePersonalDetail />
+            </Element>
+          </Columns.Column>
+          <Columns.Column size={6}>
+            <EditableLanguages />
+          </Columns.Column>
+        </Columns>
+      </Element>
+
+      {userIsMentee &&
+        <Element className="block-separator">
+          <Columns>
+            <Columns.Column size={6}>
+              <Element className="block-separator">
+                <EditableEducation />
+              </Element>
             </Columns.Column>
-            <Columns.Column size={8}>
-              <Heading>Hi, {profile.firstName}</Heading>
-              <Content size="medium" renderAs="p" responsive={{ mobile: { hide: { value: true } } }}>
-                {`Please fill out your profile. Let potential ${userIsMentee ? 'mentors' : 'mentees'} know a little bit more about you, so you can find the perfect fit. If you have filled out your profile: Great! Make sure you keep it up to date.`}
-              </Content>
+            <Columns.Column size={6}>
+              <EditableRediClass />
             </Columns.Column>
           </Columns>
-          <Element className="block-separator" responsive={{ tablet: { hide: { value: true } } }}>
-            <Content size="medium" renderAs="p">
-              {`Please fill out your profile. Let potential ${userIsMentee ? 'mentors' : 'mentees'} know a little bit more about you, so you can find the perfect fit. If you have filled out your profile: Great! Make sure you keep it up to date.`}
-            </Content>
-          </Element>
-          <Element className="block-separator">
-            <EditableAbout />
-          </Element>
-
-          <Element className="block-separator">
-            <EditableMentoringTopics />
-          </Element>
-
-          <Element className="block-separator">
-            <Columns>
-              <Columns.Column size={6}>
-                <Element className="block-separator">
-                  <EditableContactDetails />
-                </Element>
-              </Columns.Column>
-              <Columns.Column size={6}>
-                <EditableSocialMedia />
-              </Columns.Column>
-            </Columns>
-          </Element>
-
-          <Element className="block-separator">
-            <Columns>
-              <Columns.Column size={6}>
-                <Element className="block-separator">
-                  <EditablePersonalDetail />
-                </Element>
-              </Columns.Column>
-              <Columns.Column size={6}>
-                <EditableLanguages />
-              </Columns.Column>
-            </Columns>
-          </Element>
-
-          {userIsMentee &&
-            <Element className="block-separator">
-              <Columns>
-                <Columns.Column size={6}>
-                  <Element className="block-separator">
-                    <EditableEducation />
-                  </Element>
-                </Columns.Column>
-                <Columns.Column size={6}>
-                  <EditableRediClass />
-                </Columns.Column>
-              </Columns>
-            </Element>
-          }
-
-          <Element className="block-separator">
-            <Columns>
-              <Columns.Column size={6}>
-                <EditableOccupation />
-              </Columns.Column>
-            </Columns>
-          </Element>
-        </>
+        </Element>
       }
+
+      <Element className="block-separator">
+        <Columns>
+          <Columns.Column size={6}>
+            <EditableOccupation />
+          </Columns.Column>
+        </Columns>
+      </Element>
     </LoggedIn>
   )
 }
