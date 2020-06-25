fc72086423755ff0b35a070a96367528890c46a2
Marcus Zierke <mzierke@deloitte.de>
2020-06-25 15:42:44 +0200

    build(front): favicon + deloitte logo + rediLogo in navbar added

--- redi-connect-front/src/components/atoms/Logo.tsx
+++ redi-connect-front/src/components/atoms/Logo.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import './Logo.scss'
+import { Element } from 'react-bulma-components'
+import { ReactComponent as RediLogo } from '../../assets/images/logo.svg'
+import { ReactComponent as RediLogoMobile } from '../../assets/images/logo-mobile.svg'
 
 const Logo = () => {
   return (
-    <a className="logo" href="/">
-      <strong>ReDI </strong>Connect
+    <a href="/">
+      <Element responsive={{ mobile: { hide: { value: true } } }}>
+        <RediLogo />
+      </Element>
+      <Element responsive={{ tablet: { hide: { value: true } } }}>
+        <RediLogoMobile />
+      </Element>
     </a>
   )
 }
