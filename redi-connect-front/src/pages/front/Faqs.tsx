7abb249e04f5d927bcce80c867dceb2070debe7b
Endre Zónai <ezonai@deloitte.de>
2020-07-31 10:42:11 +0200

    fix(front): fix text for 2 faqs and small design fixes

--- redi-connect-front/src/pages/front/Faqs.tsx
+++ redi-connect-front/src/pages/front/Faqs.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Landing from '../../components/templates/Landing'
 import { Section, Container, Columns, Element, Heading as BulmaHeading } from 'react-bulma-components'
-import { useTranslation } from 'react-i18next'
+import { useTranslation, Trans } from 'react-i18next'
 import classnames from 'classnames'
 import Icon from '../../components/atoms/Icon'
 import Heading from '../../components/atoms/Heading'
@@ -16,10 +16,9 @@ const Faq = ({ question, answer }: Props) => {
   const [showAnswer, setShowAnswer] = useState(false)
   return (
     <div className="faq">
-      <Columns breakpoint="mobile" onClick={() => setShowAnswer(!showAnswer)}>
+      <Columns breakpoint="mobile" className="faq__title" onClick={() => setShowAnswer(!showAnswer)}>
         <Columns.Column>
-          <Element>{question}
-          </Element>
+          <Element>{question}</Element>
         </Columns.Column>
         <Columns.Column narrow>
           <Icon
@@ -29,7 +28,9 @@ const Faq = ({ question, answer }: Props) => {
           />
         </Columns.Column>
       </Columns>
-      <Element className={classnames('faq__answer', { 'faq__answer--show': showAnswer })}>{answer}</Element>
+      <Element className={classnames('faq__answer', { 'faq__answer--show': showAnswer })}>
+        <div dangerouslySetInnerHTML={{ __html: `${answer}` }} />
+      </Element>
     </div>
   )
 }
@@ -48,8 +49,8 @@ const Faqs = () => {
             <Columns.Column desktop={{ size: 10, offset: 1 }}>
               <Heading size="medium" border="bottomLeft" className="double-bs">{t('faq.headline')}</Heading>
               {topics.map((topic) => (
-                <div>
-                  <BulmaHeading size={4} className="faq__title" subtitle>{topic.title}</BulmaHeading>
+                <div className="faq-block">
+                  <BulmaHeading size={4} className="faq-block__title" subtitle>{topic.title}</BulmaHeading>
                   {topic.qAndAs.map((item, index) =>
                     <Faq key={index} question={item.question} answer={item.answer} />
                   )}
