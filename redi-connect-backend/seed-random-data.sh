877d6e728ed9045718b1c9ea5e99ac8124436fe1
Eric Bolikowski <eric@binarylights.com>
2020-11-19 06:35:09 +0100

    feat: update seed script

--- redi-connect-backend/seed-random-data.sh
+++ redi-connect-backend/seed-random-data.sh
@@ -1,3 +1,3 @@
-HTTP=1 NODE_ENV=seeding node scripts/seed-database.js && 
-HTTP=1 NODE_ENV=seeding node scripts/create-roles.js &&
-HTTP=1 NODE_ENV=seeding node scripts/link-roles-to-users.js
+HTTP=1 NODE_ENV=seeding REDI_LOCATION=berlin node scripts/seed-database.js && 
+HTTP=1 NODE_ENV=seeding REDI_LOCATION=berlin node scripts/create-roles.js &&
+HTTP=1 NODE_ENV=seeding REDI_LOCATION=berlin node scripts/link-roles-to-users.js
