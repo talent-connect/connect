6100ccf99da5cdf2944c23bfd2943c16791d8afa
Eric Bolikowski <eric@binarylights.com>
2021-03-01 23:26:52 +0100

    feat: mentoring topics change + email scripts

--- redi-connect-front/src/types/Category.ts
+++ redi-connect-front/src/types/Category.ts
@@ -1,5 +1,11 @@
 export type Category = {
-  id: string
-  label: string
-  group: 'coding' | 'careerSupport' | 'other'
+  id: string;
+  label: string;
+  group:
+    | 'softwareEngineering'
+    | 'design'
+    | 'otherProfessions'
+    | 'careerSupport'
+    | 'language'
+    | 'other';
 };
