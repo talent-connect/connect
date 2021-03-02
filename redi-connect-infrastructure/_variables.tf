b9d3e9a1729fe697e6d1ced079f51061d2377fb3
Christos Paschalidis <paschalidi@outlook.com>
2021-03-02 09:36:31 +0000

    feat: creates two environments one staging and one production  (#252)
    * feat: creates staging and production environments using workspaces
    
    * chore: adds tf
    
    * chore: adds url

--- redi-connect-infrastructure/_variables.tf
+++ redi-connect-infrastructure/_variables.tf
@@ -4,11 +4,6 @@ variable "organisation" {
   default     = "redi"
 }
 
-variable "environment" {
-  type        = string
-  description = "This variable defines the environment we want terraform to apply changes into"
-}
-
 variable "location" {
   type        = string
   description = "Azure region where resources will be created"
@@ -30,4 +25,5 @@ variable "tier" {
 variable "sku_size" {
   type        = string
   description = "The SKU size of a new app service plan. The options are: F1, D1, B1, B2, B3, S1, S2, S3, P1v2, P2v2, P3v2."
+  default     = "S1"
 }
