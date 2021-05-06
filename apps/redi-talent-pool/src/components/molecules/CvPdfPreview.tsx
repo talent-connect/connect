62156ef6c15baf43353cc449bbff5d2096b3fb42
Eric Bolikowski <eric@binarylights.com>
2021-05-06 19:19:39 +0200

    feat: prep for react-pdf

--- apps/redi-talent-pool/src/components/molecules/CvPdfPreview.tsx
+++ apps/redi-talent-pool/src/components/molecules/CvPdfPreview.tsx
@@ -6,7 +6,9 @@ import {
   StyleSheet,
   PDFViewer,
   Image,
+  usePDF,
 } from '@react-pdf/renderer'
+import { useEffect, useState } from 'react'
 
 export interface UserCVData {
   position: string
@@ -60,14 +62,27 @@ const styles = StyleSheet.create({
   },
 })
 
-const CVPDFPreview = ({
-  cvData: { firstName, lastName, position, profileImage },
-}: CVPDFPreviewProps) => {
+// const CVPDF = ({
+//   cvData: { firstName, lastName, position, profileImage },
+// }: CVPDFPreviewProps) => {
+const CVPDF = () => {
+  const firstName = 'Eric',
+    lastName = 'Bolikowski',
+    position = 'yo ho',
+    profileImage =
+      'https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+  // const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   setInterval(() => setCount((count) => count + 1), 2000)
+  // }, [])
   return (
     <Document title={`${firstName}_${lastName}_CV.pdf`}>
       <Page size="A4" style={styles.page}>
         <View style={styles.header}>
-          <Text style={styles.headerText1}>{position}</Text>
+          <Text style={styles.headerText1}>
+            {/* {count} */}
+            {position}
+          </Text>
           <Text style={styles.headerText2}>{firstName}</Text>
           <Text style={styles.headerText3}>{lastName}</Text>
           <Image style={styles.headerImg} src={profileImage} />
@@ -75,6 +90,16 @@ const CVPDFPreview = ({
       </Page>
     </Document>
   )
+}
+
+const CVPDFPreview = ({
+  cvData: { firstName, lastName, position, profileImage },
+  styles,
+}: CVPDFPreviewProps & { styles?: any }) => {
+  const [instance, updateInstance] = usePDF({ document: <CVPDF /> })
+  const [count, setCount] = useState(0)
+  console.log(instance.blob)
+  return <div style={styles}>hello</div>
 }
 
 export default CVPDFPreview
--- apps/redi-talent-pool/src/pages/app/cv-wizard/CVWizardContainer.tsx
+++ apps/redi-talent-pool/src/pages/app/cv-wizard/CVWizardContainer.tsx
@@ -24,22 +24,13 @@ const UserData = {
     'https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
 }
 
-const styles = StyleSheet.create({
-  viewer: {
-    width: '50vw',
-    height: '100vh',
-  },
-})
-
 export function CVWizardContainer(props: CVWizardContainerProps) {
   return (
     <LoggedIn>
       <Container className="cv-wizard-container">
         <Columns>
           <Columns.Column size={6} className="column--side-menu">
-            <PDFViewer>
-              <CVPDFPreview cvData={UserData} />
-            </PDFViewer>
+            <CVPDFPreview cvData={UserData} />
           </Columns.Column>
           <Columns.Column size={6} className="column--main-content">
             main content
