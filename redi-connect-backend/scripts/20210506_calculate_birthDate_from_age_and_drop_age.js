b77353b7e55d435695e63fa01c04b6903b1781a8
Eric Bolikowski <eric@binarylights.com>
2021-05-17 11:48:44 +0200

    feat(backend): migration script case for age not set

--- redi-connect-backend/scripts/20210506_calculate_birthDate_from_age_and_drop_age.js
+++ redi-connect-backend/scripts/20210506_calculate_birthDate_from_age_and_drop_age.js
@@ -5,16 +5,25 @@ const app = require('../server/server');
 
 const RedProfile = app.models.RedProfile;
 
-(async () => {
+async function doStuff(){
   const allProfiles = await RedProfile.find({ include: 'redUser' });
   allProfiles.forEach(async (profile) => {
-    const toDay = moment(new Date());
-    const birthDate = moment((toDay.format('DD.MM.') + (toDay.year() - profile.age).toString()), 'DD.MM.YYYY')
-    console.log('this RedProfile :');
-    console.log(`${profile.firstName} ${profile.lastName}`);
-    console.log('birthDate calculated:');
-    console.log(`${birthDate} from age ${profile.age}`);
-    await profile.updateAttribute('birthDate', birthDate);
-    await profile.removeAttribute('age');
+    const age = profile.age;
+    await profile.updateAttribute('age', null);
+
+    if (age) {
+      const toDay = moment(new Date());
+      const birthDate = moment((toDay.format('DD.MM.') + (toDay.year() - age).toString()), 'DD.MM.YYYY')
+      console.log('this RedProfile :');
+      console.log(`${profile.firstName} ${profile.lastName}`);
+      console.log('birthDate calculated:');
+      console.log(`${birthDate} from age ${age}`);
+      await profile.updateAttribute('birthDate', birthDate);
+    } else {
+      await profile.updateAttribute('birthDate', null);
+    }
+    
   });
-})();
+}
+
+doStuff()
