445e55556d0d87335026c83b8588eef11b172029
Eric Bolikowski <eric@binarylights.com>
2021-05-19 12:11:42 +0200

    chore: remove dead code

--- apps/admin-panel/src/components/component-factories/build-approve-or-reject-button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Button from '@material-ui/core/Button'
-
-export const buildApproveOrRejectButton = () => {}
-
-export const ApproveRejectButton = () => {
-  return <Button>Hello</Button>
-}
