ee93630feef3e34ac1ee0a0a5ebe106b57c053e5
Eric Bolikowski <eric@binarylights.com>
2021-06-16 06:41:28 +0200

    chore: enable react-query dev tools

--- apps/redi-talent-pool/src/App.tsx
+++ apps/redi-talent-pool/src/App.tsx
@@ -1,6 +1,7 @@
 import { Loader } from '@talent-connect/shared-atomic-design-components'
 import { Suspense } from 'react'
 import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import AppNotification from './components/AppNotification'
 import { Routes } from './components/Routes'
 import { queryClient } from './services/api/api'
@@ -9,6 +10,7 @@ import { history, Router } from './services/history/history'
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <AppNotification />
       <Router history={history}>
         <Suspense fallback={<Loader loading={true} />}>
