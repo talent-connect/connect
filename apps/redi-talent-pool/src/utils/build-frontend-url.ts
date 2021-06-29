f54480e836b4a572c8b1b0b2101d9b3b659147b4
Anil <aak@hellofresh.com>
2021-06-30 00:05:52 +0200

    Fix unreachable code

--- apps/redi-talent-pool/src/utils/build-frontend-url.ts
+++ apps/redi-talent-pool/src/utils/build-frontend-url.ts
@@ -15,8 +15,6 @@ export function buildFrontendUrl(env: string, rediLocation: RediLocation) {
     return 'https://app.staging.connect.redi-school.org'
   } else if (env === 'development') {
     return 'http://127.0.0.1:3000'
-  } else if (env === 'development') {
-    return 'http://127.0.0.1:3000'
   } else {
     return 'http://127.0.0.1:3000'
   }
