83d027f16b82403d5ba42bc9e178f082c26332b3
Eric Bolikowski <eric@binarylights.com>
2021-07-02 22:41:08 +0200

    feat(tp): insert links

--- apps/redi-talent-pool/src/components/templates/LoggedIn.tsx
+++ apps/redi-talent-pool/src/components/templates/LoggedIn.tsx
@@ -2,6 +2,8 @@ import { Loader } from '@talent-connect/shared-atomic-design-components'
 import React, { ReactNode } from 'react'
 import { Columns, Container, Section } from 'react-bulma-components'
 import { useIsBusy } from '../../hooks/useIsBusy'
+import { useTpCompanyProfileQuery } from '../../react-query/use-tpcompanyprofile-query'
+import { useTpJobseekerProfileQuery } from '../../react-query/use-tpjobseekerprofile-query'
 import { TpMainNavItem } from '../molecules/TpMainNavItem'
 import { Navbar } from '../organisms'
 import Footer from '../organisms/Footer'
@@ -13,6 +15,10 @@ interface Props {
 
 const LoggedIn = ({ children }: Props) => {
   const isBusy = useIsBusy()
+  const { data: jobseekerProfile } = useTpJobseekerProfileQuery({
+    retry: false,
+  })
+  const { data: companyProfile } = useTpCompanyProfileQuery({ retry: false })
 
   return (
     <>
@@ -29,15 +35,17 @@ const LoggedIn = ({ children }: Props) => {
             <TpMainNavItem
               page="browse-page"
               pageName="Browse"
-              to="https://www.google.com"
+              to="/app/browse"
               isDisabled
             />
-            <TpMainNavItem
-              page="cv-builder-page"
-              pageName="CV Builder"
-              to="https://www.google.com"
-              isDisabled
-            />
+            {jobseekerProfile ? (
+              <TpMainNavItem
+                page="cv-builder-page"
+                pageName="CV Builder"
+                to="/app/cv-builder"
+                isDisabled
+              />
+            ) : null}
           </div>
           <div className="main-container--horizontal-spacer"></div>
           <Columns style={{ width: '100%', marginTop: '1rem' }}>
