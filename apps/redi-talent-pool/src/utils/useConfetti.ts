fc722b950591701ded2065f317e39540ea972119
Anil <aak@hellofresh.com>
2021-07-07 20:18:13 +0200

    Add firing confetti in home page

---
+++ apps/redi-talent-pool/src/utils/useConfetti.ts
@@ -0,0 +1,48 @@
+import mousetrap from './mousetrap'
+import confetti from 'canvas-confetti'
+
+const duration = 10 * 1000
+const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+interface UseConfettiConfig {
+  keybind?: string
+}
+
+const defaultUseConfettiConfig = {
+  keybind: 'c o n f e t t i',
+}
+
+export function useConfetti(
+  config: UseConfettiConfig = defaultUseConfettiConfig
+) {
+  mousetrap.bind(config.keybind || 'r e d i c o n f e t t i', function () {
+    const animationEnd = Date.now() + duration
+
+    const interval = setInterval(function () {
+      const timeLeft = animationEnd - Date.now()
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval)
+      }
+
+      const particleCount = 250 * (timeLeft / duration)
+      // since particles fall down, start a bit higher than random
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+      )
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+      )
+    }, 250)
+  })
+}
