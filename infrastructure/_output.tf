5957c21e190e45da35669f2257b14825664168ff
Eric Bolikowski <eric@binarylights.com>
2021-10-14 23:41:59 +0200

    chore(infra): resume infrastructure/azure work with terraform

---
+++ infrastructure/_output.tf
@@ -0,0 +1,100 @@
+output "resource_group_name" {
+  value = "rediconnect"
+  // todo when we will be having our REDI connect azure account this will have to return
+  //  value = azurerm_resource_group.webapp-rg.name
+}
+
+output "storage_account_name" {
+  value       = module.static-website-berlin.storage_account_name
+  description = "The name of the storage account."
+}
+output "storage_account_name_munich" {
+  value       = module.static-website-munich.storage_account_name
+  description = "The name of the storage account."
+}
+output "storage_account_name_picker" {
+  value       = module.static-website-picker.storage_account_name
+  description = "The name of the storage account."
+}
+output "storage_account_name_nrw" {
+  value       = module.static-website-nrw.storage_account_name
+  description = "The name of the storage account."
+}
+
+output "static_website_cdn_profile_name_berlin" {
+  value       = module.static-website-berlin.static_website_cdn_profile_name
+  description = "CDN profile name for the static website for berlin"
+}
+
+output "static_website_cdn_profile_name_munich" {
+  value       = module.static-website-munich.static_website_cdn_profile_name
+  description = "CDN profile name for the static website for munich"
+}
+
+output "static_website_cdn_profile_name_picker" {
+  value       = module.static-website-picker.static_website_cdn_profile_name
+  description = "CDN profile name for the static website for berlin"
+}
+
+output "static_website_cdn_profile_name_nrw" {
+  value       = module.static-website-nrw.static_website_cdn_profile_name
+  description = "CDN profile name for the static website for munich"
+}
+
+output "static_website_cdn_host_name_berlin" {
+  value       = module.static-website-berlin.static_website_cdn_endpoint_hostname
+  description = "CDN profile name for the static website for berlin"
+}
+
+output "static_website_cdn_host_name_munich" {
+  value       = module.static-website-munich.static_website_cdn_endpoint_hostname
+  description = "CDN profile name for the static website for munich"
+}
+
+output "static_website_cdn_host_name_picker" {
+  value       = module.static-website-picker.static_website_cdn_endpoint_hostname
+  description = "CDN profile name for the static website for berlin"
+}
+
+output "static_website_cdn_host_name_nrw" {
+  value       = module.static-website-nrw.static_website_cdn_endpoint_hostname
+  description = "CDN profile name for the static website for munich"
+}
+
+output "static_website_url_berlin" {
+  value       = module.static-website-berlin.static_website_url
+  description = "static web site URL from storage account"
+}
+
+output "static_website_url_munich" {
+  value       = module.static-website-munich.static_website_url
+  description = "static web site URL from storage account"
+}
+
+output "static_website_url_nrw" {
+  value       = module.static-website-nrw.static_website_url
+  description = "static web site URL from storage account"
+}
+
+output "static_website_url_picker" {
+  value       = module.static-website-picker.static_website_url
+  description = "static web site URL from storage account"
+}
+
+output "azure_app_service_name" {
+  value = module.web_app_container.name
+}
+
+output "azure_container_registry" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "azure_container_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "azure_container_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
---
+++ infrastructure/scripts/README.md
@@ -0,0 +1,9 @@
+# generate-tf-storage.sh
+
+Before you use Azure Storage as a back end, you must create a storage account. 
+The storage account can be created with the Azure portal, PowerShell, the Azure CLI, or Terraform itself. Use the following sample to configure the storage account with the Azure CLI.
+
+you might need to 
+```
+chmod +x generate-tf-storage.sh
+```
