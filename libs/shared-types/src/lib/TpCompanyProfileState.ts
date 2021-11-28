60210f15f65c521d84e60d1cc4462369ca5ffeff
Anil <aak@hellofresh.com>
2021-11-28 20:51:26 +0100

    Add comment explaining company profile state lifecycle

--- libs/shared-types/src/lib/TpCompanyProfileState.ts
+++ libs/shared-types/src/lib/TpCompanyProfileState.ts
@@ -1,3 +1,12 @@
+/**
+ * Although the natural lifecycle of a TpCompanyProfileState
+ * is drafting-ptofile => submitted-for-review => profile-approved,
+ * companies can be approved even if they are not submitted-for-review
+ * yet. (See PR: https://github.com/talent-connect/connect/pull/460).
+ * In some edge cases where companies reach out to a ReDI Admin and
+ * get approved, the TpCompanyProfileState can go from drafting-profile
+ * to profile-approved.
+ */
 export const TpCompanyProfileState = {
   'drafting-profile': 'drafting-profile',
   'submitted-for-review': 'submitted-for-review',
