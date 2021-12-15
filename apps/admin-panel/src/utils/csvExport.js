52f7fa1fda28572e05564714e00ad36a49f0b70f
Anil <aak@hellofresh.com>
2021-12-15 15:33:25 +0100

    [Issue 477] Add more fields to Red Matches CSV

---
+++ apps/admin-panel/src/utils/csvExport.js
@@ -0,0 +1,21 @@
+import { downloadCSV } from 'react-admin'
+import { unparse as convertToCSV } from 'papaparse/papaparse.min'
+
+export const redMatchesCsvExporter = async (redMatches) => {
+  const data = redMatches.map((redMatch) => ({
+    status: redMatch.status,
+    applicationText: redMatch.applicationText,
+    expectationText: redMatch.expectationText,
+    rediLocation: redMatch.rediLocation,
+    mentorName: `${redMatch.mentor.firstName} ${redMatch.mentor.lastName}`,
+    mentorContactEmail: redMatch.mentor.contactEmail,
+    menteeName: `${redMatch.mentee.firstName} ${redMatch.mentee.lastName}`,
+    menteeContactEmail: redMatch.mentee.contactEmail,
+  }))
+
+  const csv = convertToCSV({
+    data,
+  })
+
+  downloadCSV(csv, 'ReDI Mentorship Matches')
+}
