87778ce229d214770c6ea2aeb892bb8d8bd8a874
Pablo Perez <pablo.perez@pohlcon.com>
2021-12-16 21:43:00 +0100

    [FIX] Namespaced colors

--- libs/shared-atomic-design-components/.storybook/main.ts
+++ libs/shared-atomic-design-components/.storybook/main.ts
@@ -3,7 +3,8 @@ const rootMain = require('../../../.storybook/main')
 // Use the following syntax to add addons!
 // rootMain.addons.push('');
 rootMain.stories.push(
-  ...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)']
+  '../src/lib/**/*.stories.mdx',
+  '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
 )
 
 module.exports = rootMain
--- libs/shared-atomic-design-components/src/lib/atoms/Checkbox/Checkbox.spec.ts
+++ libs/shared-atomic-design-components/src/lib/atoms/Checkbox/Checkbox.spec.ts
@@ -1,4 +1,4 @@
-import {Checkbox} from '.';
+import { Checkbox } from '.';
 
 describe(Checkbox.name, () => {
   it('passes', () => {
