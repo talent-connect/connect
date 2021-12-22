4a9faa3906a3de6d9aacb0b7535fc7bbbccc6677
Anil <aak@hellofresh.com>
2021-12-23 00:28:43 +0300

    Add more fields in redMatchesCsvExporter

--- apps/admin-panel/src/utils/csvExport.js
+++ apps/admin-panel/src/utils/csvExport.js
@@ -4,14 +4,18 @@ import { unparse as convertToCSV } from 'papaparse/papaparse.min'
 export const redMatchesCsvExporter = async (redMatches) => {
   const data = redMatches.map((redMatch) => ({
     status: redMatch.status,
-    applicationText: redMatch.applicationText,
-    expectationText: redMatch.expectationText,
     rediLocation: redMatch.rediLocation,
     mentorName: `${redMatch.mentor?.firstName} ${redMatch.mentor?.lastName}`,
     mentorContactEmail: redMatch.mentor?.contactEmail,
     menteeName: `${redMatch.mentee?.firstName} ${redMatch.mentee?.lastName}`,
     menteeContactEmail: redMatch.mentee?.contactEmail,
+    applicationText: redMatch.applicationText,
+    expectationText: redMatch.expectationText,
+    createdAt: redMatch.createdAt,
+    mentorReplyMessageOnAccept: redMatch.mentorReplyMessageOnAccept,
     matchCompletedOn: redMatch.matchCompletedOn,
+    mentorMessageOnComplete: redMatch.mentorMessageOnComplete,
+    matchMadeActiveOn: redMatch.matchMadeActiveOn,
   }))
 
   const csv = convertToCSV({
