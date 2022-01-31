3cd789d90d97c5fdb7bed9b08108e58577dcdfcb
Eric Bolikowski <eric@binarylights.com>
2022-01-31 16:53:15 +0100

    feat(con): create new data lists

---
+++ libs/shared-config/src/lib/mentoring-data-lists.ts
@@ -0,0 +1,12 @@
+export const MENTORING_GOALS = {
+  abc: 'ABC',
+  abc_plus: 'ABC+',
+  abc_plus_plus: 'ABC++',
+  abc_plus_plus_plus: 'ABC+++',
+} as const
+
+export const PROFESSIONAL_EXPERIENCE_FIELDS = {
+  none: 'None',
+  some: 'Some',
+  professional: 'Professional',
+} as const
