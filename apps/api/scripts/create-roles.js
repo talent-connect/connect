c0478bd35d8bd11a8b5c376821886bc93b10605f
lorenzo <lorepirri@gmail.com>
2022-02-19 09:20:12 +0100

    [chore] roles log pipe sections

--- apps/api/scripts/create-roles.js
+++ apps/api/scripts/create-roles.js
@@ -1,5 +1,5 @@
 const { bindNodeCallback, from } = require('rxjs')
-const { concatMap } = require('rxjs/operators')
+const { concatMap, tap } = require('rxjs/operators')
 
 const app = require('../server/server')
 const { Role } = app.models
@@ -8,6 +8,13 @@ const roleCreate = bindNodeCallback(Role.create.bind(Role))
 
 const roles = ['admin', 'mentee', 'mentor', 'jobseeker', 'company']
 
+console.log('****** Create Roles **************************')
 from(roles)
-  .pipe(concatMap((role) => roleCreate({ name: role })))
-  .subscribe(console.log, null, () => process.exit())
+  .pipe(
+    tap((role) => console.log(`role ${role}`)),
+    concatMap((role) => roleCreate({ name: role }))
+  )
+  .subscribe(console.log, null, () => {
+    console.log('*** DONE Create Roles ************************')
+    process.exit()
+  })
