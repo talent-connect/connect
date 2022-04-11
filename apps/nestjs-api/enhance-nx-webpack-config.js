6fe363ccb0c7110e32f997f78facaff377192e88
Eric Bolikowski <eric@binarylights.com>
2022-04-11 16:16:07 +0200

    set up nest-cli decorator codegen for graphql entity/ts files

---
+++ apps/nestjs-api/enhance-nx-webpack-config.js
@@ -0,0 +1,35 @@
+// Thanks goes to https://github.com/nrwl/nx/issues/4135 for this
+
+// TL:DR: this uses a feature of NestJS's CLI to look up all GraphQL model/TS files
+// (e.g. .entity.ts, see config below) and decorate them.
+// More about this feature here:  https://docs.nestjs.com/graphql/cli-plugin
+
+module.exports = function (config, context) {
+  const tsLoader = config.module.rules.find((r) =>
+    r.loader.includes('ts-loader')
+  )
+
+  if (tsLoader) {
+    tsLoader.options.transpileOnly = false
+    tsLoader.options.getCustomTransformers = (program) => {
+      return {
+        before: [
+          require('@nestjs/graphql/plugin').before(
+            {
+              typeFileNameSuffix: [
+                '.input.ts',
+                '.args.ts',
+                '.entity.ts',
+                '.model.ts',
+              ],
+              introspectComments: true,
+            },
+            program
+          ),
+        ],
+      }
+    }
+  }
+
+  return config
+}
