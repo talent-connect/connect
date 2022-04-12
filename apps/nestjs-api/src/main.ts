46b148541d90b0da2eed5a34f981bc96bd0b13df
Eric Bolikowski <eric@binarylights.com>
2022-04-12 13:07:01 +0200

    increase request limits

--- apps/nestjs-api/src/main.ts
+++ apps/nestjs-api/src/main.ts
@@ -5,13 +5,15 @@
 
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-
+import { json, urlencoded } from 'express'
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
+  app.use(json({ limit: '1mb' }))
+  app.use(urlencoded({ extended: true, limit: '1mb' }))
   const port = process.env.PORT || 3333
   await app.listen(port)
   Logger.log(
