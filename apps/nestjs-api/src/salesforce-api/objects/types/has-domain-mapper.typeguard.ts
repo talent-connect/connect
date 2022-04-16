f583b7d939eb70f3e044404b5f38541d50fa7c6e
Eric Bolikowski <eric@binarylights.com>
2022-04-16 17:39:43 +0200

    remove unused stuff

--- apps/nestjs-api/src/salesforce-api/objects/types/has-domain-mapper.typeguard.ts
+++
@@ -1,8 +0,0 @@
-import { BaseDomainModel } from '../../../core/models/generics/base-domain.model'
-import { ToDomainMapper } from './to-domain-mapper.interface'
-
-function hasDomainMapper<T extends BaseDomainModel>(
-  sfObject: any
-): sfObject is ToDomainMapper<T> {
-  return 'toDomain' in sfObject
-}
