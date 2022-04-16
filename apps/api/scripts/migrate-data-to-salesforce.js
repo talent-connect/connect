00da58066dabeee6b6f81fe9da10e721c893a4f0
Eric Bolikowski <eric@binarylights.com>
2022-04-16 16:11:46 +0200

    data migration script changes

--- apps/api/scripts/migrate-data-to-salesforce.js
+++ apps/api/scripts/migrate-data-to-salesforce.js
@@ -38,8 +38,8 @@ const conn = new jsforce.Connection({
   clientSecret: CLIENT_SECRET,
 })
 
-const REDPROFILE_SFCONTACT = {}
-const REDPROFILE_SFCONNECTPROFILE = {}
+let REDPROFILE_SFCONTACT = {}
+let REDPROFILE_SFCONNECTPROFILE = {}
 
 const MENTOR = '0129X0000001EXBQA2'
 const MENTEE = '0129X0000001EYnQAM'
@@ -62,12 +62,14 @@ const {
   count,
 } = require('rxjs/operators')
 
+const { of } = require('rxjs')
+
 const LANGUAGES = require('./languages')
 const { merge } = require('lodash')
 
-function insertContact(p) {}
-
-function insertConnectProfile(p) {}
+const DELAY = 2500
+const RETRIES = 10
+const CONCURRENCY = 10
 
 function retryWithDelay(delayTime, count = 1) {
   return (input) =>
@@ -87,6 +89,89 @@ function retryWithDelay(delayTime, count = 1) {
         )
       )
     )
+}
+
+async function insertContactFn(p) {
+  const result = await conn.sobject('Contact').create({
+    // AccountId: '0019X000002URWKQA4',
+    RecordTypeId: '0121i000000HMq9AAG',
+    FirstName: `${p.firstName}`,
+    LastName: `${p.lastName}`,
+    redi_Contact_Gender__c: p.gender ? p.gender.toUpperCase() : 'OTHER',
+    ReDI_Birth_Date__c: p.birthDate,
+    LinkedIn_Profile__c: p.linkedInProfileUrl,
+    ReDI_GitHub_Profile__c: p.githubProfileUrl,
+    ReDI_Slack_Username__c: p.slackUsername,
+    MobilePhone: p.telephoneNumber,
+    // Name: `${p.firstName} ${p.lastName}`,
+  })
+  return { ...p, sfContactId: result.id }
+}
+
+function insertContact(p) {
+  return of(p).pipe(
+    switchMap((x) => from(insertContactFn(p))),
+    retryWithDelay(DELAY, RETRIES)
+  )
+}
+async function insertConnectProfileFn(p) {
+  const result = await conn.sobject('ReDI_Connect_Profile__c').create({
+    Contact__c: p.sfContactId,
+    RecordTypeId: p.userType.indexOf('mentor') !== -1 ? MENTOR : MENTEE,
+    Profile_Status__c: redProfileToProfileStatus(p),
+    ReDI_Location__c: p.rediLocation,
+    Occupation__c: p.mentor_occupation,
+
+    Work_Place__c: p.mentor_workPlace,
+    Expectations__c: p.expectations
+      ? p.expectations.substr(0, 1000)
+      : undefined,
+    Occupation_Category__c: p.mentee_occupationCategoryId,
+    Place_of_Employment__c: p.mentee_occupationJob_placeOfEmployment,
+    Job_Title__c: p.mentee_occupationJob_position,
+    Study_Place__c: p.mentee_occupationStudent_studyPlace,
+    Study_Name__c: p.mentee_occupationStudent_studyName,
+    Desired_Job__c: p.mentee_occupationLookingForJob_what,
+    Main_Occupation_Other__c: p.mentee_occupationOther_description,
+    Education__c: p.mentee_highestEducationLevel,
+    ReDI_Course__c: p.mentee_currentlyEnrolledInCourse || 'alumni',
+    Avatar_Image_URL__c: p.profileAvatarImageS3Key,
+    Personal_Description__c: p.personalDescription
+      ? p.personalDescription.substr(0, 600)
+      : undefined,
+    Languages__c: p.languages
+      ? p.languages.map((langLabel) => LANGUAGES[langLabel]).join(';')
+      : undefined,
+    Opt_Out_Mentees_From_Other_Locations__c:
+      p.optOutOfMenteesFromOtherRediLocation,
+    Profile_First_Approved_At__c: p.userActivatedAt,
+    // CreatedDate: p.createdAt, //! Use Jonida trick
+    // LastModifiedDate: p.updatedAt, //! Use Jonida trick
+  })
+  return { ...p, sfConnectProfileId: result.id }
+}
+function insertConnectProfile(p) {
+  return of(p).pipe(
+    switchMap((x) => from(insertConnectProfileFn(p))),
+    retryWithDelay(DELAY, RETRIES)
+  )
+}
+
+async function insertMentoringSessionFn(p) {
+  console.log('trying to insert mentoring session', p.id)
+  const result = await conn.sobject('Mentoring_Session__c').create({
+    Date__c: p.date,
+    Mentee__c: REDPROFILE_SFCONTACT[p.menteeId],
+    Mentor__c: REDPROFILE_SFCONTACT[p.mentorId],
+    Durations_in_Minutes__c: p.minuteDuration,
+  })
+  return { ...p, sfId: result.id }
+}
+function insertMentoringSession(p) {
+  return of(p).pipe(
+    switchMap((x) => from(insertMentoringSessionFn(p))),
+    retryWithDelay(DELAY, RETRIES)
+  )
 }
 
 ;(async () => {
@@ -120,114 +205,44 @@ function retryWithDelay(delayTime, count = 1) {
 
   await conn.login(USERNAME, `${PASSWORD}${SECURITY_TOKEN}`)
 
-  await from(allConProfiles)
-    .pipe(
-      filter((p) => p['signup-source'] != 'manual-import-via-script'),
-      mergeMap(
-        (p) =>
-          from(
-            conn.sobject('Contact').create({
-              // AccountId: '0019X000002URWKQA4',
-              RecordTypeId: '0121i000000HMq9AAG',
-              FirstName: `${p.firstName}`,
-              LastName: `${p.lastName}`,
-              redi_Contact_Gender__c: p.gender
-                ? p.gender.toUpperCase()
-                : 'OTHER',
-              ReDI_Birth_Date__c: p.birthDate,
-              LinkedIn_Profile__c: p.linkedInProfileUrl,
-              ReDI_GitHub_Profile__c: p.githubProfileUrl,
-              ReDI_Slack_Username__c: p.slackUsername,
-              MobilePhone: p.telephoneNumber,
-              // Name: `${p.firstName} ${p.lastName}`,
-            })
-          ).pipe(
-            retryWhen((errors) =>
-              errors.pipe(delay(10000), take(30), concatMap(throwError))
-            )
-          ),
-        (p, { id }) => {
-          return { ...p, sfContactId: id }
-        },
-        10
-      ),
-      tap((p) => console.log('Inserted Contact #', p.sfContactId)),
-      mergeMap(
-        (p) =>
-          from(
-            conn.sobject('ReDI_Connect_Profile__c').create({
-              Contact__c: p.sfContactId,
-              RecordTypeId:
-                p.userType.indexOf('mentor') !== -1 ? MENTOR : MENTEE,
-              Profile_Status__c: redProfileToProfileStatus(p),
-              ReDI_Location__c: p.rediLocation,
-              Occupation__c: p.mentor_occupation,
+  // await from(allConProfiles)
+  //   .pipe(
+  //     filter((p) => p['signup-source'] != 'manual-import-via-script'),
+  //     map((p) => {
+  //       if (
+  //         p.mentee_currentlyEnrolledInCourse === 'munich_frontendDevelopment'
+  //       ) {
+  //         p.mentee_currentlyEnrolledInCourse = 'munich_frontend1'
+  //       }
+  //       return p
+  //     }),
+  //     mergeMap((p) => insertContact(p), CONCURRENCY),
+  //     tap((p) => console.log('Inserted Contact #', p.sfContactId)),
+  //     mergeMap((p) => insertConnectProfile(p), CONCURRENCY),
+  //     tap((p) => console.log('Inserted ConProfile #', p.sfConnectProfileId)),
+  //     tap(({ id, sfContactId, sfConnectProfileId }) => {
+  //       REDPROFILE_SFCONTACT[id] = sfContactId
+  //       REDPROFILE_SFCONNECTPROFILE[id] = sfConnectProfileId
+  //     }),
+  //     scan((acc, curr) => acc + 1, 0),
+  //     tap(console.log)
+  //   )
+  //   .toPromise()
 
-              Work_Place__c: p.mentor_workPlace,
-              Expectations__c: p.expectations
-                ? p.expectations.substr(0, 1000)
-                : undefined,
-              Occupation_Category__c: p.mentee_occupationCategoryId,
-              Place_of_Employment__c: p.mentee_occupationJob_placeOfEmployment,
-              Job_Title__c: p.mentee_occupationJob_position,
-              Study_Place__c: p.mentee_occupationStudent_studyPlace,
-              Study_Name__c: p.mentee_occupationStudent_studyName,
-              Desired_Job__c: p.mentee_occupationLookingForJob_what,
-              Main_Occupation_Other__c: p.mentee_occupationOther_description,
-              Education__c: p.mentee_highestEducationLevel,
-              ReDI_Course__c: p.mentee_currentlyEnrolledInCourse || 'alumni',
-              Avatar_Image_URL__c: p.profileAvatarImageS3Key,
-              Personal_Description__c: p.personalDescription
-                ? p.personalDescription.substr(0, 600)
-                : undefined,
-              Languages__c: p.languages
-                ? p.languages.map((langLabel) => LANGUAGES[langLabel]).join(';')
-                : undefined,
-              Opt_Out_Mentees_From_Other_Locations__c:
-                p.optOutOfMenteesFromOtherRediLocation,
-              Profile_First_Approved_At__c: p.userActivatedAt,
-              // CreatedDate: p.createdAt, //! Use Jonida trick
-              // LastModifiedDate: p.updatedAt, //! Use Jonida trick
-            })
-          ).pipe(
-            retryWhen((errors) =>
-              errors.pipe(delay(10000), take(30), concatMap(throwError))
-            )
-          ),
-        (p, { id }) => {
-          return { ...p, sfConnectProfileId: id }
-        },
-        10
-      ),
-      tap((p) => console.log('Inserted ConProfile #', p.sfConnectProfileId)),
-      tap(({ id, sfContactId, sfConnectProfileId }) => {
-        REDPROFILE_SFCONTACT[id] = sfContactId
-        REDPROFILE_SFCONNECTPROFILE[id] = sfConnectProfileId
-      }),
-      scan((acc, curr) => acc + 1, 0),
-      tap(console.log)
-    )
-    .toPromise()
+  // fs.writeFileSync('./map.json', JSON.stringify(REDPROFILE_SFCONTACT))
+
+  const json = fs.readFileSync('./map.json')
+
+  REDPROFILE_SFCONTACT = JSON.parse(json)
 
   await from(allConMentoringSessions)
     .pipe(
-      mergeMap(
+      filter((p) => p.mentorId && p.menteeId),
+      filter(
         (p) =>
-          from(
-            conn.sobject('Mentoring_Session__c').create({
-              Date__c: p.date,
-              Mentee__c: REDPROFILE_SFCONTACT[p.menteeId],
-              Mentor__c: REDPROFILE_SFCONTACT[p.mentorId],
-              Durations_in_Minutes__c: p.minuteDuration,
-            })
-          ).pipe(
-            retryWhen((errors) =>
-              errors.pipe(delay(10000), take(30), concatMap(throwError))
-            )
-          ),
-        (s, { id }) => ({ ...s, sfId: id }),
-        10
+          REDPROFILE_SFCONTACT[p.mentorId] && REDPROFILE_SFCONTACT[p.menteeId]
       ),
+      mergeMap((p) => insertMentoringSession(p), CONCURRENCY),
       tap((p) => console.log('Inserted Mentoring Session #', p.sfId)),
       scan((acc, curr) => acc + 1, 0),
       tap(console.log)
@@ -245,52 +260,3 @@ function redProfileToProfileStatus(redProfile) {
 
   return 'APPROVED'
 }
-
-/*
-
-
-
-const redUserFind = (q) => bindNodeCallback(RedUser.find.bind(RedUser))(q)
-
-redUserFind({ include: 'redProfile' })
-  .pipe(
-    delay(1000),
-    switchMap((users) => from(users)),
-    map((data) => {
-      const pojo = JSON.parse(JSON.stringify(data))
-      return {
-        redUser: pojo,
-        redProfile: pojo.redProfile,
-        redUserInst: data,
-        redProfileInst: data.redProfile,
-      }
-    }),
-    filter(({ redProfile }) => !!redProfile),
-    filter(({ redProfile }) => redProfile.userType === 'mentor'),
-    filter(({ redProfile }) => redProfile.userActivated),
-
-    // take(2),
-    // map((data, index) => {
-    //   data.redProfile.contactEmail =
-    //     index === 0 ? 'miriam@redi-school.org' : 'johanna@redi-school.org'
-    //   return data
-    // }),
-    concatMap(
-      (userData) =>
-        sendEmail({
-          recipient: userData.redProfile.contactEmail,
-          firstName: userData.redProfile.firstName,
-        }).pipe(delay(500)),
-
-      (userData, sendResult) => ({ ...userData, sendResult })
-    ),
-
-    tap((userData) => console.log(userData.redProfile.contactEmail)),
-    count()
-  )
-  .subscribe(
-    (count) => console.log('did this ' + count + ' times'),
-    (e) => console.log('Error: ', e),
-    () => console.log('done')
-  )
-*/
