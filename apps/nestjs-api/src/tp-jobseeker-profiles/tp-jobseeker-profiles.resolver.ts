435fb6daeb7496be9f19d3614521f83d8ee2d9b2
Eric Bolikowski <eric@binarylights.com>
2022-08-29 11:21:43 +0200

    fix errors

--- apps/nestjs-api/src/tp-jobseeker-profiles/tp-jobseeker-profiles.resolver.ts
+++ apps/nestjs-api/src/tp-jobseeker-profiles/tp-jobseeker-profiles.resolver.ts
@@ -1,23 +1,19 @@
-import { UseGuards } from '@nestjs/common';
-import { Resolver } from '@nestjs/graphql';
-import { GqlJwtAuthGuard } from '../auth/gql-jwt-auth.guard';
-import { TpJobseekerProfilesService } from './tp-jobseeker-profiles.service';
+import { UseGuards } from '@nestjs/common'
+import { GqlJwtAuthGuard } from '../auth/gql-jwt-auth.guard'
+import { TpJobseekerProfilesService } from './tp-jobseeker-profiles.service'
 
 @UseGuards(GqlJwtAuthGuard)
-@Resolver(() => TpJobseekerProfileEntityProps)
+// @Resolver(() => TpJobseekerProfileEntityProps)
 export class TpJobseekerProfilesResolver {
-  constructor(
-    private readonly service: TpJobseekerProfilesService
-  ) {}
-}
+  constructor(private readonly service: TpJobseekerProfilesService) {}
 
-  //! TODO: Add auth
-  @Query(() => [TpJobseekerEntityProps], {
-    name: 'tpJobseekerProfiles',
-  })
-  async findAllVisible() {
-    const entities = await this.service.findAll({})
-    const props = entities.map((entity) => entity.props)
-    return props
-  }
+  // //! TODO: Add auth
+  // @Query(() => [TpJobseekerEntityProps], {
+  //   name: 'tpJobseekerProfiles',
+  // })
+  // async findAllVisible() {
+  //   const entities = await this.service.findAll({})
+  //   const props = entities.map((entity) => entity.props)
+  //   return props
+  // }
 }
