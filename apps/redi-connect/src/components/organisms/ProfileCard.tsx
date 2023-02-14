bc70f750b57cd99960065c830cdfa063bb9bdde9
kate <kathrin.voronina@gmail.com>
2023-02-14 13:58:42 +0100

    Make a mentor profile card to open in a new tab

--- apps/redi-connect/src/components/organisms/ProfileCard.tsx
+++ apps/redi-connect/src/components/organisms/ProfileCard.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import classnames from 'classnames'
+import { NavLink } from 'react-router-dom'
 import { Card, Element, Tag } from 'react-bulma-components'
 import { Icon, PipeList } from '@talent-connect/shared-atomic-design-components'
 
-import { useHistory } from 'react-router-dom'
 import {
   AWS_PROFILE_AVATARS_BUCKET_BASE_URL,
   REDI_LOCATION_NAMES,
@@ -28,7 +27,6 @@ const ProfileCard = ({
   toggleFavorite,
   isFavorite,
 }: ProfileCardProps) => {
-  const history = useHistory()
 
   const {
     firstName,
@@ -44,49 +42,47 @@ const ProfileCard = ({
     toggleFavorite && toggleFavorite(profile.id)
   }
 
-  const handleLinkTo = () => linkTo && history.push(linkTo)
-
   const imgSrc = profileAvatarImageS3Key
     ? AWS_PROFILE_AVATARS_BUCKET_BASE_URL + profileAvatarImageS3Key
     : placeholderImage
 
   return (
-    <Card
-      className={classnames('profile-card', { 'profile-card--active': linkTo })}
-      onClick={linkTo ? handleLinkTo : undefined}
-    >
-      <Card.Image
-        className="profile-card__image"
-        src={imgSrc}
-        alt={`${firstName} ${lastName}`}
-      />
-      <Card.Content>
-        {toggleFavorite && (
-          <div className="profile-card__favorite" onClick={handleFavorite}>
-            <Icon
-              icon={isFavorite ? 'heartFilled' : 'heart'}
-              className="profile-card__favorite__icon"
-            />
-          </div>
-        )}
-        <Element
-          key="name"
-          renderAs="h3"
-          textWeight="bold"
-          textSize={4}
-          className="profile-card__name"
-        >
-          {firstName} {lastName}
-        </Element>
-        <Element key="location" renderAs="span" className="content">
-          {REDI_LOCATION_NAMES[rediLocation]}
-        </Element>
-        {languages && <PipeList items={languages} />}
-        {categories && (
-          <ReadMentoringTopics.Tags items={categories} shortList />
-        )}
-      </Card.Content>
-    </Card>
+    <NavLink to={linkTo} className="profile-card-link">
+      <Card className="profile-card">
+        <Card.Image
+          className="profile-card__image"
+          src={imgSrc}
+          alt={`${firstName} ${lastName}`}
+        />
+        <Card.Content>
+          {toggleFavorite && (
+            <div className="profile-card__favorite" onClick={handleFavorite}>
+              <Icon
+                icon={isFavorite ? 'heartFilled' : 'heart'}
+                className="profile-card__favorite__icon"
+              />
+            </div>
+          )}
+          <Element
+            key="name"
+            renderAs="h3"
+            textWeight="bold"
+            textSize={4}
+            className="profile-card__name"
+          >
+            {firstName} {lastName}
+          </Element>
+          <Element key="location" renderAs="span" className="content">
+            {REDI_LOCATION_NAMES[rediLocation]}
+          </Element>
+          {languages && <PipeList items={languages} />}
+          {categories && (
+            <ReadMentoringTopics.Tags items={categories} shortList />
+          )}
+        </Card.Content>
+      </Card>
+    </NavLink>
+    
   )
 }
 
