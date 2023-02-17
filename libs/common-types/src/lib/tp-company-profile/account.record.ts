9fc85a0ded7dd661b3b7a71b966f7aa09905a1fc
Eric Bolikowski <eric@binarylights.com>
2023-02-17 15:45:36 +0100

    ReDI_Is_Job_Fair_2023_Participant__c

--- libs/common-types/src/lib/tp-company-profile/account.record.ts
+++ libs/common-types/src/lib/tp-company-profile/account.record.ts
@@ -31,6 +31,7 @@ export class AccountRecord extends Record<AccountRecordProps> {
       'Description',
       'ReDI_Talent_Pool_State__c',
       'ReDI_Visible_to_Jobseekers__c',
+      'ReDI_Is_Job_Fair_2023_Participant__c',
     ],
   }
 }
