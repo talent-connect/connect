a6943a9b6bea4e24960ab0120964e3ebcc63ab24
Eric Bolikowski <eric@binarylights.com>
2023-02-23 23:16:54 +0800

    <MeCompany>: organize imports + ordering of misc. funcs

--- apps/redi-talent-pool/src/pages/app/me/MeCompany.tsx
+++ apps/redi-talent-pool/src/pages/app/me/MeCompany.tsx
@@ -1,94 +1,25 @@
-import React, { useCallback, useState } from 'react'
 import { Tooltip } from '@material-ui/core'
 import {
   Button,
   Checkbox,
   Icon,
 } from '@talent-connect/shared-atomic-design-components'
-import { Columns, Notification, Content } from 'react-bulma-components'
-import { EditableAbout } from '../../../components/organisms/company-profile-editables/EditableAbout'
-import { EditableContact } from '../../../components/organisms/company-profile-editables/EditableContact'
-import { EditableDetails } from '../../../components/organisms/company-profile-editables/EditableDetails'
-import { EditableNamePhotoLocation } from '../../../components/organisms/company-profile-editables/EditableNamePhotoLocation'
-import { EditableJobPostings } from '../../../components/organisms/company-profile-editables/EditableJobPostings'
-import { LoggedIn } from '../../../components/templates'
-import { useTpCompanyProfileQuery } from '../../../react-query/use-tpcompanyprofile-query'
-import { useTpCompanyProfileUpdateMutation } from '../../../react-query/use-tpcompanyprofile-mutation'
-import { useTpJobListingAllQuery } from '../../../react-query/use-tpjoblisting-all-query'
 import {
   TpCompanyProfile,
   TpCompanyProfileState,
 } from '@talent-connect/shared-types'
+import { useCallback, useState } from 'react'
+import { Columns, Content, Notification } from 'react-bulma-components'
+import { EditableAbout } from '../../../components/organisms/company-profile-editables/EditableAbout'
+import { EditableContact } from '../../../components/organisms/company-profile-editables/EditableContact'
+import { EditableDetails } from '../../../components/organisms/company-profile-editables/EditableDetails'
+import { EditableJobPostings } from '../../../components/organisms/company-profile-editables/EditableJobPostings'
+import { EditableNamePhotoLocation } from '../../../components/organisms/company-profile-editables/EditableNamePhotoLocation'
+import { LoggedIn } from '../../../components/templates'
+import { useTpCompanyProfileUpdateMutation } from '../../../react-query/use-tpcompanyprofile-mutation'
+import { useTpCompanyProfileQuery } from '../../../react-query/use-tpcompanyprofile-query'
+import { useTpJobListingAllQuery } from '../../../react-query/use-tpjoblisting-all-query'
 import { OnboardingSteps } from './TpCompanyProfileOnboardingSteps'
-
-function isProfileComplete(profile: Partial<TpCompanyProfile>): boolean {
-  const requiredSectionsComplete = [
-    EditableNamePhotoLocation.isSectionFilled,
-    EditableNamePhotoLocation.isPhotoSelected,
-    EditableContact.isSectionFilled,
-    EditableDetails.isWebsiteSectionFilled,
-  ]
-    .map((checkerFn) => checkerFn(profile))
-    .every((p) => p)
-
-  return requiredSectionsComplete
-}
-
-function SendProfileForReviewButton() {
-  const { data: profile } = useTpCompanyProfileQuery()
-  const { data: jobListings } = useTpJobListingAllQuery()
-  const mutation = useTpCompanyProfileUpdateMutation()
-
-  const enabled =
-    profile?.state === 'drafting-profile' &&
-    isProfileComplete(profile) &&
-    jobListings?.length > 0
-
-  const getTooltipText = () => {
-    if (profile.state !== 'submitted-for-review') {
-      return isProfileComplete(profile)
-        ? 'You need to post a job listing before you can send it for review'
-        : 'You need to complete your profile before you can send it for review'
-    }
-
-    return 'Your profile is currently being reviewed'
-  }
-
-  const onClick = useCallback(() => {
-    if (!window.confirm('Would you like to submit your profile for review?'))
-      return
-
-    mutation.mutate({ ...profile, state: 'submitted-for-review' })
-  }, [mutation, profile])
-
-  if (enabled) {
-    return <Button onClick={onClick}>Send profile to review</Button>
-  } else {
-    return (
-      <Tooltip title={getTooltipText()}>
-        <span>
-          <Button disabled style={{ pointerEvents: 'none' }}>
-            Send profile to review
-          </Button>
-        </span>
-      </Tooltip>
-    )
-  }
-}
-
-const CallToActionButton = ({
-  profile,
-}: {
-  profile: Partial<TpCompanyProfile>
-}) =>
-  profile &&
-  profile.state &&
-  [
-    TpCompanyProfileState['drafting-profile'],
-    TpCompanyProfileState['submitted-for-review'],
-  ].includes(profile.state as any) ? (
-    <SendProfileForReviewButton />
-  ) : null
 
 export function MeCompany() {
   const { data: profile } = useTpCompanyProfileQuery()
@@ -181,3 +112,72 @@ export function MeCompany() {
     </LoggedIn>
   )
 }
+
+function isProfileComplete(profile: Partial<TpCompanyProfile>): boolean {
+  const requiredSectionsComplete = [
+    EditableNamePhotoLocation.isSectionFilled,
+    EditableNamePhotoLocation.isPhotoSelected,
+    EditableContact.isSectionFilled,
+    EditableDetails.isWebsiteSectionFilled,
+  ]
+    .map((checkerFn) => checkerFn(profile))
+    .every((p) => p)
+
+  return requiredSectionsComplete
+}
+
+function SendProfileForReviewButton() {
+  const { data: profile } = useTpCompanyProfileQuery()
+  const { data: jobListings } = useTpJobListingAllQuery()
+  const mutation = useTpCompanyProfileUpdateMutation()
+
+  const enabled =
+    profile?.state === 'drafting-profile' &&
+    isProfileComplete(profile) &&
+    jobListings?.length > 0
+
+  const getTooltipText = () => {
+    if (profile.state !== 'submitted-for-review') {
+      return isProfileComplete(profile)
+        ? 'You need to post a job listing before you can send it for review'
+        : 'You need to complete your profile before you can send it for review'
+    }
+
+    return 'Your profile is currently being reviewed'
+  }
+
+  const onClick = useCallback(() => {
+    if (!window.confirm('Would you like to submit your profile for review?'))
+      return
+
+    mutation.mutate({ ...profile, state: 'submitted-for-review' })
+  }, [mutation, profile])
+
+  if (enabled) {
+    return <Button onClick={onClick}>Send profile to review</Button>
+  } else {
+    return (
+      <Tooltip title={getTooltipText()}>
+        <span>
+          <Button disabled style={{ pointerEvents: 'none' }}>
+            Send profile to review
+          </Button>
+        </span>
+      </Tooltip>
+    )
+  }
+}
+
+const CallToActionButton = ({
+  profile,
+}: {
+  profile: Partial<TpCompanyProfile>
+}) =>
+  profile &&
+  profile.state &&
+  [
+    TpCompanyProfileState['drafting-profile'],
+    TpCompanyProfileState['submitted-for-review'],
+  ].includes(profile.state as any) ? (
+    <SendProfileForReviewButton />
+  ) : null
