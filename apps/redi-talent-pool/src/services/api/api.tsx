9bda120b61b4a6b7749c928103113eb17c08c517
kate <kathrin.voronina@gmail.com>
2023-03-01 12:12:05 +0100

    Display job postings in chronological order

--- apps/redi-talent-pool/src/services/api/api.tsx
+++ apps/redi-talent-pool/src/services/api/api.tsx
@@ -358,13 +358,12 @@ export async function fetchAllTpJobListingsUsingFilters({
 
 export async function fetchAllTpJobListings(): Promise<Array<TpJobListing>> {
   const userId = getAccessTokenFromLocalStorage().userId
-  const resp = await http(`${API_URL}/redUsers/${userId}/tpJobListings`)
-
-  // TODO: remove the `.filter()`. It
-  // was inserted temporarily for the "dummy" job listings we created for HR Summit
-  // 2021. Once the event is over, they can be removed from database completely.
-  // Reason for filter here is so companies don't see these dummy job listings.
-  return resp.data.filter((listing) => !listing.dummy)
+  return http(
+    `${API_URL}/redUsers/${userId}/tpJobListings?filter=${JSON.stringify({
+      order: 'createdAt DESC',
+      limit: 0,
+    })}`
+  ).then((resp) => resp.data)
 }
 
 export async function fetchOneTpJobListingOfCurrentUser(
