55ef3d91060dff6775691dcf0a3bfef3d60d6cfc
Eric Bolikowski <eric@binarylights.com>
2023-03-26 13:44:36 +0700

    fix(con/tp): auth layer

--- apps/redi-connect/src/services/auth/auth.ts
+++ apps/redi-connect/src/services/auth/auth.ts
@@ -2,7 +2,9 @@ import { graphqlClient } from '@talent-connect/data-access'
 import { AccessToken } from '@talent-connect/shared-types'
 
 export const isLoggedIn = (): boolean => {
-  const accessToken: any = window.localStorage.getItem('accessToken')
+  const accessToken: any = window.localStorage.getItem(
+    'postSalesforceMigrationAccessToken'
+  )
   try {
     const r2: any = JSON.parse(accessToken)
     return Boolean(r2)
@@ -13,10 +15,15 @@ export const isLoggedIn = (): boolean => {
 
 // TODO: add | undefined here...
 export const getAccessTokenFromLocalStorage = (): AccessToken =>
-  JSON.parse(window.localStorage.getItem('accessToken') as string)
+  JSON.parse(
+    window.localStorage.getItem('postSalesforceMigrationAccessToken') as string
+  )
 
 export const saveAccessTokenToLocalStorage = (accessToken: AccessToken) => {
-  window.localStorage.setItem('accessToken', JSON.stringify(accessToken))
+  window.localStorage.setItem(
+    'postSalesforceMigrationAccessToken',
+    JSON.stringify(accessToken)
+  )
 }
 
 export function setGraphQlClientAuthHeader(accessToken: AccessToken) {
--- apps/redi-talent-pool/src/services/auth/auth.ts
+++ apps/redi-talent-pool/src/services/auth/auth.ts
@@ -3,7 +3,9 @@ import { AccessToken, RedUser } from '@talent-connect/shared-types'
 import { queryClient } from '../api/api'
 
 export const isLoggedIn = (): boolean => {
-  const accessToken: any = window.localStorage.getItem('accessToken')
+  const accessToken: any = window.localStorage.getItem(
+    'postSalesforceMigrationAccessToken'
+  )
   try {
     const r1: any = JSON.parse(accessToken)
     return Boolean(r1)
@@ -13,17 +15,27 @@ export const isLoggedIn = (): boolean => {
 }
 
 export const getRedUserFromLocalStorage = (): RedUser =>
-  JSON.parse(window.localStorage.getItem('redUser') as string)
+  JSON.parse(
+    window.localStorage.getItem('postSalesforceMigrationRedUser') as string
+  )
 
 export const saveRedUserToLocalStorage = (redUser: RedUser) => {
-  window.localStorage.setItem('redUser', JSON.stringify(redUser))
+  window.localStorage.setItem(
+    'postSalesforceMigrationRedUser',
+    JSON.stringify(redUser)
+  )
 }
 
 export const getAccessTokenFromLocalStorage = (): AccessToken =>
-  JSON.parse(window.localStorage.getItem('accessToken') as string)
+  JSON.parse(
+    window.localStorage.getItem('postSalesforceMigrationAccessToken') as string
+  )
 
 export const saveAccessTokenToLocalStorage = (accessToken: AccessToken) => {
-  window.localStorage.setItem('accessToken', JSON.stringify(accessToken))
+  window.localStorage.setItem(
+    'postSalesforceMigrationAccessToken',
+    JSON.stringify(accessToken)
+  )
 }
 
 export function setGraphQlClientAuthHeader(accessToken: AccessToken) {
