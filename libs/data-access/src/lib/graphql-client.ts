fb35bb100bed8bd01d6beaf62b8f88e5a267bc05
Anıl Akarsu <anil@redi-school.org>
2023-05-11 09:05:11 +0200

    [CON/TP]: Handle the "unauthorized error" case when the JWT token expires (#736)
    * Fix [CON/TP]: Handle the "unauthorized error" case when the JWT token expires #728
    
    Co-authored-by: katamatata
    
    * feat: jwt expiry after 21d, not 7d
    
    ---------
    
    Co-authored-by: Eric Bolikowski <eric@binarylights.com>

--- libs/data-access/src/lib/graphql-client.ts
+++ libs/data-access/src/lib/graphql-client.ts
@@ -11,13 +11,39 @@ export const graphqlClient = new GraphQLClient(endpoint, {
   // },
 })
 
+// Middleware to handle 401 errors
+const handleUnauthorizedError = (error: any) => {
+  if (error?.response?.errors?.length > 0) {
+    const responseError = error.response.errors[0]
+
+    if (responseError?.extensions) {
+      const extensionResponse = responseError.extensions.response
+
+      if (extensionResponse?.statusCode === 401) {
+        alert('Your session has expired. Please log in again.')
+        window.localStorage.clear()
+        window.location.replace('/front/login')
+      }
+    }
+  }
+}
+
 export function fetcher<TData, TVariables>(
   query: string,
   variables?: TVariables,
   headers?: RequestInit['headers']
 ) {
-  return async (): Promise<TData> =>
-    graphqlClient.request<TData, TVariables>(query, variables, headers)
+  return async (): Promise<TData> => {
+    return new Promise((resolve, reject) => {
+      graphqlClient
+        .request<TData, TVariables>(query, variables, headers)
+        .then(resolve)
+        .catch((error) => {
+          handleUnauthorizedError(error)
+          reject(error)
+        })
+    })
+  }
 }
 
 // export type TestQueryVariables = Types.Exact<{ [key: string]: never }>
