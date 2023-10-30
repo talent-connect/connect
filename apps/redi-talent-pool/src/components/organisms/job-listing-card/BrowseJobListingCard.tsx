ad7db856709ae8df30b75480dd03a0c9adf77b5e
helloanil <anil.akarsu@easyquant.de>
2023-10-30 08:18:44 +0100

    Fix joblistings have underline when browse

--- apps/redi-talent-pool/src/components/organisms/job-listing-card/BrowseJobListingCard.tsx
+++ apps/redi-talent-pool/src/components/organisms/job-listing-card/BrowseJobListingCard.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import './BrowseJobListingCard.scss'
 import { JobListingCard, JobListingCardProps } from './JobListingCard'
 
 interface BrowseJobListingCardProps extends JobListingCardProps {
