3775ab1c44ae6e5a2022ca6f0c5cfb3961b6250b
Rebeca Kipper <rebecakipper@gmail.com>
2023-11-24 17:32:09 +0100

    make card tags dynamic an responsive

--- libs/shared-atomic-design-components/src/lib/atoms/CardTags.tsx
+++ libs/shared-atomic-design-components/src/lib/atoms/CardTags.tsx
@@ -1,6 +1,7 @@
 import { Tag } from 'react-bulma-components'
 import './CardTags.scss'
-
+import React, { useState, useEffect } from 'react'
+import { debounce, throttle } from 'lodash'
 export interface CardTagsProps {
   items: string[]
   shortList?: boolean
@@ -13,50 +14,87 @@ interface CardTagProps {
   key: string
 }
 
-const CardTag = ({ children, className }: CardTagProps) => (
-  <>
-    <Tag
-      className={className}
-      size="normal"
-      responsive={{ tablet: { hide: { value: true } } }}
-    >
-      {children}
-    </Tag>
-    <Tag
-      className={className}
-      size="medium"
-      responsive={{ mobile: { hide: { value: true } } }}
-    >
-      {children}
-    </Tag>
-  </>
-)
-
 const CardTags = ({ items, shortList, formatter }: CardTagsProps) => {
-  const additionalTagsCount = items.length - 3
-  const tagList = shortList ? items.slice(0, 3) : items
-  const hasAdditionalTags = shortList && additionalTagsCount > 0
+  const [visibleChips, setVisibleChips] = useState([])
+  const [remainingChips, setRemainingChips] = useState([])
+
+  const parentRef = React.useRef<HTMLDivElement>()
+
+  useEffect(() => {
+    const formattedItems = items.map(formatter)
+
+    const calculateChips = () => {
+      console.log('calculating wowwwwww')
+      const parentWidth = parentRef.current?.offsetWidth
+      const gapSize = 16 // Adjust this based on the design (16px gap)
+      const sidePadding = 12 // Adjust this based on the design (12px padding each side )
+      const chipTotalMargin = 2 * sidePadding + gapSize
+
+      const chipWidths = formattedItems.map((chip) => {
+        const chipSpan = document.createElement('span')
+        chipSpan.style.visibility = 'hidden'
+        chipSpan.classList.add('chip')
+        chipSpan.innerHTML = chip
+
+        parentRef.current.appendChild(chipSpan)
+        console.log('text', chip, chipSpan.offsetWidth)
+        const width = chipSpan.offsetWidth + chipTotalMargin
+
+        parentRef.current.removeChild(chipSpan)
+
+        return width
+      })
+
+      let totalWidth = 0
+      let visibleChipsCount = 0
+
+      for (let i = 0; i < chipWidths.length; i++) {
+        totalWidth += chipWidths[i]
+
+        if (totalWidth - gapSize <= parentWidth) {
+          visibleChipsCount = i + 1
+        } else {
+          break
+        }
+      }
+
+      setVisibleChips(formattedItems.slice(0, visibleChipsCount))
+      setRemainingChips(formattedItems.slice(visibleChipsCount))
+    }
+
+    const throttledCalculateChips = debounce(calculateChips, 100)
+
+    throttledCalculateChips()
+
+    // Recalculate on window resize
+    window.addEventListener('resize', throttledCalculateChips)
+
+    return () => {
+      window.removeEventListener('resize', throttledCalculateChips)
+    }
+  }, [formatter, items])
 
   return (
-    <Tag.Group>
-      {tagList.map((tagId, i) => {
-        const currentTag = (
-          <CardTag key={tagId}>{formatter ? formatter(tagId) : tagId}</CardTag>
-        )
-        const isLastVisibleTag = i === 2
+    <>
+      <div className="wrapper__mobile">
+        {items.map((chip) => (
+          <p key={chip} className="chip">
+            {formatter(chip)}
+          </p>
+        ))}
+      </div>
 
-        return hasAdditionalTags && isLastVisibleTag ? (
-          <div className="tags__last-row" key={tagId}>
-            {currentTag}
-            <CardTag key={`restNr-${i}`} className="tag--rest">
-              {'+' + additionalTagsCount}
-            </CardTag>
-          </div>
-        ) : (
-          currentTag
-        )
-      })}
-    </Tag.Group>
+      <div className="wrapper__desktop" ref={parentRef}>
+        {visibleChips.map((chip) => (
+          <p key={chip} className="chip">
+            {chip}
+          </p>
+        ))}
+        {remainingChips.length > 0 && (
+          <p className="chip">+{remainingChips.length}</p>
+        )}
+      </div>
+    </>
   )
 }
 
