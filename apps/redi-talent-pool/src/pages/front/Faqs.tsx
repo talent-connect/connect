3ef3c1977cb78ac315902be7cd89615f5cdeb98c
Emily Yetsko <emilyyetsko@gmail.com>
2023-11-27 12:57:16 +0100

    added FAQ link to home page, updated all info on FAQ page to reflect TP questions, added to route, added files for pages/front under talent pool

---
+++ apps/redi-talent-pool/src/pages/front/Faqs.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Landing from '../../components/templates/Landing'
+import {
+  Section,
+  Container,
+  Columns,
+  Element,
+  Heading as BulmaHeading,
+} from 'react-bulma-components'
+import { useTranslation } from 'react-i18next'
+import classnames from 'classnames'
+import './Faqs.scss'
+import {
+  Heading,
+  FaqItem,
+} from '@talent-connect/shared-atomic-design-components'
+
+const Faqs = () => {
+  const { t } = useTranslation()
+
+  const topics: Array<{ title: string; qAndAs: Array<any> }> = t('faq.topics', {
+    returnObjects: true,
+  })
+
+  return (
+    <Landing>
+      <Section>
+        <Container>
+          <Columns>
+            <Columns.Column desktop={{ size: 10, offset: 1 }}>
+              <Heading size="medium" border="bottomLeft" className="double-bs">
+                {t('faq.headline')}
+              </Heading>
+              {topics.map((topic) => (
+                <div className="faq-block">
+                  <BulmaHeading size={4} className="faq-block__title" subtitle>
+                    {topic.title}
+                  </BulmaHeading>
+                  {topic.qAndAs.map((item, index) => (
+                    <FaqItem
+                      key={index}
+                      question={item.question}
+                      answer={item.answer}
+                    />
+                  ))}
+                </div>
+              ))}
+            </Columns.Column>
+          </Columns>
+        </Container>
+      </Section>
+    </Landing>
+  )
+}
+
+export default Faqs
