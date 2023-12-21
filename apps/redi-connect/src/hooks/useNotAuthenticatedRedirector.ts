40082a0fa2d0f7fb33d855bdce3cd6afaab4d8ac
Eric Bolikowski <eric@binarylights.com>
2023-12-21 18:09:15 +0100

    feat(con/tp): direct to intended page after login

--- apps/redi-connect/src/hooks/useNotAuthenticatedRedirector.ts
+++ apps/redi-connect/src/hooks/useNotAuthenticatedRedirector.ts
@@ -3,7 +3,18 @@ import { history } from '../services/history/history'
 
 export const useNotAuthenticatedRedirector = () => {
   if (!isLoggedIn()) {
-    history.replace('/front/login')
+    // For some reason code execution reaches this if branch
+    // even when .pathname is /front/login, which defeats the
+    // purpose of using history.location.pathname. We therefore
+    // only do a history.replace if the pathname starts with
+    // /app, meaning access to the authenticated/"private"
+    // part of the app.
+    const pathnameIsInPrivateArea = history.location.pathname.startsWith('/app')
+    if (pathnameIsInPrivateArea) {
+      history.replace(
+        `/front/login?goto=${encodeURIComponent(history.location.pathname)}`
+      )
+    }
     return { isRedirectingToLogin: true }
   }
   return { isRedirectingToLogin: false }
