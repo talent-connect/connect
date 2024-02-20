04c4b8f2e3d64138adf61c0729e8b88f84cd0d20
Rebeca Kipper <rebecakipper@gmail.com>
2024-02-20 12:36:55 +0100

    add new ProfileCard to JobseekerProfileCard

--- apps/redi-talent-pool/src/components/organisms/JobseekerProfileCard.tsx
+++ apps/redi-talent-pool/src/components/organisms/JobseekerProfileCard.tsx
@@ -4,11 +4,11 @@ import {
   topSkillsIdToLabelMap,
 } from '@talent-connect/talent-pool/config'
 import React from 'react'
-import { Card, Element, Tag } from 'react-bulma-components'
-import { NavLink } from 'react-router-dom'
 import placeholderImage from '../../assets/img-placeholder.png'
 import { JobseekerProfileCardJobseekerProfilePropFragment } from './JobseekerProfileCard.generated'
 import './JobseekerProfileCard.scss'
+import { ProfileCard } from '../../../../../libs/shared-atomic-design-components/src/lib/atoms/ProfileCard'
+
 interface JobseekerProfileCardProps {
   jobseekerProfile: JobseekerProfileCardJobseekerProfilePropFragment
   linkTo?: string
@@ -22,70 +22,12 @@ export function JobseekerProfileCard({
   toggleFavorite,
   isFavorite,
 }: JobseekerProfileCardProps) {
-  const fullName = jobseekerProfile?.fullName
-  const desiredPositions =
-    jobseekerProfile?.desiredPositions
-      ?.map((position) => desiredPositionsIdToLabelMap[position])
-      .join(', ') ?? ''
-  const topSkills = jobseekerProfile?.topSkills
-
-  const handleFavoriteClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-    toggleFavorite && toggleFavorite(jobseekerProfile.id)
-  }
-
-  const imgSrc = jobseekerProfile?.profileAvatarImageS3Key
-    ? jobseekerProfile?.profileAvatarImageS3Key
-    : placeholderImage
-
   return (
-    <NavLink to={linkTo} className="jobseeker-profile-link">
-      <Card className="jobseeker-profile-card">
-        <Card.Image
-          className="jobseeker-profile-card__image"
-          src={imgSrc}
-          alt={fullName}
-        />
-        <Card.Content>
-          {jobseekerProfile.isHired ? (
-            <Tag className="jobseeker-profile-card__hired-tag">HIRED!</Tag>
-          ) : null}
-          {toggleFavorite && (
-            <div
-              className="jobseeker-profile-card__favorite"
-              onClick={handleFavoriteClick}
-            >
-              <Icon
-                icon={isFavorite ? 'heartFilled' : 'heart'}
-                className="jobseeker-profile-card__favorite__icon"
-              />
-            </div>
-          )}
-          <Element
-            key="name"
-            renderAs="h3"
-            textWeight="bold"
-            textSize={4}
-            className="jobseeker-profile-card__job-title"
-          >
-            {fullName}
-          </Element>
-          <Element
-            className="content jobseeker-profile-card__company-name"
-            key="location"
-            renderAs="div"
-          >
-            {desiredPositions}
-          </Element>
-          {topSkills?.length > 0 ? (
-            <CardTags
-              items={topSkills}
-              shortList
-              formatter={(skill: string) => topSkillsIdToLabelMap[skill]}
-            />
-          ) : null}
-        </Card.Content>
-      </Card>
-    </NavLink>
+    <ProfileCard
+      profile={jobseekerProfile}
+      linkTo={linkTo}
+      isFavorite={isFavorite}
+      toggleFavorite={toggleFavorite}
+    />
   )
 }
