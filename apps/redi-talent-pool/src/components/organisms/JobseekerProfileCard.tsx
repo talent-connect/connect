c635547072c98b7d86deeae6fc1edd4b9eeb2ac7
Rebeca Kipper <62472348+rebecakipper@users.noreply.github.com>
2024-03-12 23:55:23 +0100

    Tp con/improve design job seeker and mentor cards (#876)
    * adjust cardTags design
    
    * create shared ProfileCard
    
    * add icons for profilecard
    
    * transfer CardLocation to shared components
    
    * add new ProfileCard to JobseekerProfileCard
    
    * add new ProfileCards
    
    * add language + location to JobsekerProfile fragment
    
    * move component to molecules and rename from ProfileCard
    
    * add logic to process props at JonseekerProfileCard
    
    * improve mobile tag styles and logic
    
    * add newProfileCard and responsiveness to mentor list
    
    * add newProfileCard and responsiveness to mentor list (corrected)
    
    * create MentorProfileCard

--- apps/redi-talent-pool/src/components/organisms/JobseekerProfileCard.tsx
+++ apps/redi-talent-pool/src/components/organisms/JobseekerProfileCard.tsx
@@ -1,14 +1,12 @@
-import { CardTags, Icon } from '@talent-connect/shared-atomic-design-components'
 import {
   desiredPositionsIdToLabelMap,
   topSkillsIdToLabelMap,
 } from '@talent-connect/talent-pool/config'
-import React from 'react'
-import { Card, Element, Tag } from 'react-bulma-components'
-import { NavLink } from 'react-router-dom'
 import placeholderImage from '../../assets/img-placeholder.png'
 import { JobseekerProfileCardJobseekerProfilePropFragment } from './JobseekerProfileCard.generated'
 import './JobseekerProfileCard.scss'
+import { NewProfileCard } from '../../../../../libs/shared-atomic-design-components/src/lib/molecules/NewProfileCard'
+
 interface JobseekerProfileCardProps {
   jobseekerProfile: JobseekerProfileCardJobseekerProfilePropFragment
   linkTo?: string
@@ -22,70 +20,36 @@ export function JobseekerProfileCard({
   toggleFavorite,
   isFavorite,
 }: JobseekerProfileCardProps) {
-  const fullName = jobseekerProfile?.fullName
-  const desiredPositions =
-    jobseekerProfile?.desiredPositions
-      ?.map((position) => desiredPositionsIdToLabelMap[position])
-      .join(', ') ?? ''
-  const topSkills = jobseekerProfile?.topSkills
+  const {
+    id,
+    profileAvatarImageS3Key,
+    fullName,
+    desiredPositions,
+    location,
+    workingLanguages,
+    topSkills,
+    isHired,
+  } = jobseekerProfile
 
-  const handleFavoriteClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-    toggleFavorite && toggleFavorite(jobseekerProfile.id)
-  }
+  const subheader = desiredPositions
+    ?.map((position) => desiredPositionsIdToLabelMap[position])
+    .join(', ')
 
-  const imgSrc = jobseekerProfile?.profileAvatarImageS3Key
-    ? jobseekerProfile?.profileAvatarImageS3Key
-    : placeholderImage
+  const languages = workingLanguages?.map(({ language }) => language)
+  const tags = topSkills.map((skill) => topSkillsIdToLabelMap[skill])
+  const avatar = profileAvatarImageS3Key || placeholderImage
 
   return (
-    <NavLink to={linkTo} className="jobseeker-profile-link">
-      <Card className="jobseeker-profile-card">
-        <Card.Image
-          className="jobseeker-profile-card__image"
-          src={imgSrc}
-          alt={fullName}
-        />
-        <Card.Content>
-          {jobseekerProfile.isHired ? (
-            <Tag className="jobseeker-profile-card__hired-tag">HIRED!</Tag>
-          ) : null}
-          {toggleFavorite && (
-            <div
-              className="jobseeker-profile-card__favorite"
-              onClick={handleFavoriteClick}
-            >
-              <Icon
-                icon={isFavorite ? 'heartFilled' : 'heart'}
-                className="jobseeker-profile-card__favorite__icon"
-              />
-            </div>
-          )}
-          <Element
-            key="name"
-            renderAs="h3"
-            textWeight="bold"
-            textSize={4}
-            className="jobseeker-profile-card__job-title"
-          >
-            {fullName}
-          </Element>
-          <Element
-            className="content jobseeker-profile-card__company-name"
-            key="location"
-            renderAs="div"
-          >
-            {desiredPositions}
-          </Element>
-          {topSkills?.length > 0 ? (
-            <CardTags
-              items={topSkills}
-              shortList
-              formatter={(skill: string) => topSkillsIdToLabelMap[skill]}
-            />
-          ) : null}
-        </Card.Content>
-      </Card>
-    </NavLink>
+    <div className="jobSeeker-profile-card-wrapper">
+      <NewProfileCard
+        profile={{ id, avatar, fullName, location, languages }}
+        subheader={subheader}
+        tags={tags}
+        topChip={isHired ? 'Hired!' : ''}
+        linkTo={linkTo}
+        isFavorite={isFavorite}
+        toggleFavorite={toggleFavorite}
+      />
+    </div>
   )
 }
---
+++ libs/shared-atomic-design-components/src/lib/atoms/CardLocation.tsx
@@ -0,0 +1,22 @@
+import LocationIcon from '../../assets/images/Location.svg'
+import './CardLocation.scss'
+
+const CardLocation = ({ location, remote }) => {
+  const locationArr = location.split(',')
+  const newLocationsString =
+    locationArr.length > 3
+      ? locationArr.slice(0, 3).join(',') + '...'
+      : location
+
+  return (
+    <div className="card__location-container">
+      <img src={LocationIcon} alt="Location" className="card__location-icon" />
+      <p className="card__location-text">
+        {newLocationsString}
+        {remote ? ' | Remote' : ''}
+      </p>
+    </div>
+  )
+}
+
+export default CardLocation
