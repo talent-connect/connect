8c1786f7f37607e011b497518d3f8873551405d4
katamatata <kathrin.voronina@gmail.com>
2024-03-18 19:14:04 +0100

    Fix sendMentorSignupCompleteEmail

--- apps/nestjs-api/src/con-profiles/con-profiles-salesforce-event-handler.service.ts
+++ apps/nestjs-api/src/con-profiles/con-profiles-salesforce-event-handler.service.ts
@@ -102,6 +102,7 @@ export class ConProfilesSalesforceEventHandlerService {
           recipient: conProfile.props.email,
           firstName: conProfile.props.firstName,
           isPartnershipMentor: conProfile.props.mentor_isPartnershipMentor,
+          rediLocation: conProfile.props.rediLocation,
         })
         break
     }
