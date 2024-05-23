e011880433d2b200e93ba84c87bd4ee9a28fe5c0
katamatata <kathrin.voronina@gmail.com>
2024-05-23 16:03:15 +0200

    Setup shadcn/ui, wip

--- apps/redi-connect/src/pages/app/me/Me.tsx
+++ apps/redi-connect/src/pages/app/me/Me.tsx
@@ -110,20 +110,14 @@ function Me() {
   return (
     <LoggedIn>
       {/* testing Tailwind classes */}
-      <div className="text-2xl bg-purple-200 text-red-500">
-        Hello TailwindCSS
-      </div>
-      <div className="text-3xl font-bold underline">Hello TailwindCSS</div>
       <div className="bg-indigo-500 p-2 font-mono text-white">
         Hello TailwindCSS
       </div>
       <div className="bg-purple-200">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Ready to dive in?</span>
-            <span className="block text-indigo-600">
-              Start your free trial today.
-            </span>
+            <span className="block">Ready to TailwindCSS?</span>
+            <span className="block text-indigo-600">Start today.</span>
           </h2>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md shadow">
