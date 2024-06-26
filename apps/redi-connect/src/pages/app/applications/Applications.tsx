5981c00929b048b04f60d94dc387cc9a04e32a5d
katamatata <kathrin.voronina@gmail.com>
2024-06-26 20:10:04 +0200

    Remove comments and pseudo code

--- apps/redi-connect/src/pages/app/applications/Applications.tsx
+++ apps/redi-connect/src/pages/app/applications/Applications.tsx
@@ -3,7 +3,6 @@ import {
   useLoadMyProfileQuery,
 } from '@talent-connect/data-access'
 import { Heading } from '@talent-connect/shared-atomic-design-components'
-import { useRef } from 'react'
 import { Content } from 'react-bulma-components'
 import { useHistory } from 'react-router-dom'
 import LoggedIn from '../../../components/templates/LoggedIn'
@@ -13,35 +12,7 @@ import { ApplicationsFilterContextProvider } from './ApplicationsFilterContext'
 import DesktopView from './DesktopView'
 import MobileView from './MobileView'
 
-/*
-
-1st render:
-applicants = [a(pending)]
-previousRenderApplicants.current = [a(pending)]
-
-2nd render (line 31):
-applicants = [a(invalidated)]
-previousRenderApplicants.current = [a(pending)]
-
-previousNumberOfApplied = 1
-previousNumberOfInvalidated = 0
-currentNumberOfApplied = 0
-currentNumberOfInvalidated = 1
-
-did the number of applications with status "applied" go down by one between last render and this render?
-
-and, did the number of applications with status "invalidated" go up by one between last render and this render?
-
-if answer to both questions is YES, then we set largeBooleanVar = true
-
-2nd render (line 35):
-applicants = [a(invalidated)]
-previousRenderApplicants.current = = applicants = [a(invalidated)]
-
-*/
-
 function Applications() {
-  const previousRenderApplicants = useRef<null | any[]>(null)
   const mentorshipMatchesQuery = useGetMentorshipMatchesQuery(
     {},
     { refetchInterval: 60 * 1000 }
@@ -59,27 +30,11 @@ function Applications() {
 
   const applicants = mentorshipMatchesQuery.data?.conMentorshipMatches
 
-  let hasOneOfTheMentorshipMatchesChangedStatusFromAppliedToInvalidatedBetweenPreviousRenderAndThisRender =
-    false
-
-  // We have a complex if condition we need to write here. Some of the elements to consider:
-  // - previousRenderApplicants.current == null, don't proceed: longBooleanVar = false
-  // - find the number of applications with status = applied, and the number of applications with
-  //   status invalidated. Do this for previousRenderApplicants and applicants.
-
-  previousRenderApplicants.current = applicants
-
   return (
     <LoggedIn>
       <Heading subtitle size="small" className="double-bs">
         Applications
       </Heading>
-      {hasOneOfTheMentorshipMatchesChangedStatusFromAppliedToInvalidatedBetweenPreviousRenderAndThisRender ? (
-        <p>
-          Yo, sorry about that, one of the applications that you saw on this
-          page for mentorship with you was invalidated
-        </p>
-      ) : null}
       {applicants?.length !== undefined && (
         <>
           {applicants.length === 0 ? (
