0657e86509228c165588c75f2b007bd3222cec50
katamatata <kathrin.voronina@gmail.com>
2024-07-12 22:38:57 +0200

    Add Nx utility function for TailwindCSS purging

--- apps/redi-talent-pool/tailwind.config.js
+++ apps/redi-talent-pool/tailwind.config.js
@@ -1,8 +1,10 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind')
 const { join } = require('path')
 
 module.exports = {
   content: [
     join(__dirname, './src/**/*!(*.stories|*.spec).{js,ts,jsx,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
