a335c9e1b2abe9f920d49a34ea3c9c076af9122f
katamatata <kathrin.voronina@gmail.com>
2024-07-29 11:30:10 +0200

    Update PostCSS configuration for TailwindCSS v2

--- apps/redi-connect/postcss.config.js
+++ apps/redi-connect/postcss.config.js
@@ -1,10 +1,22 @@
-const { join } = require('path')
-
+// Configuration for TailwindCSS v2.2.16
 module.exports = {
-  plugins: {
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
-    autoprefixer: {},
-  },
+  plugins: [require('tailwindcss'), require('autoprefixer')],
 }
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+// Configuration for TailwindCSS v3
+// const { join } = require('path')
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {
+//       config: join(__dirname, 'tailwind.config.js'),
+//     },
+//     autoprefixer: {},
+//   },
+// }
--- apps/redi-talent-pool/postcss.config.js
+++ apps/redi-talent-pool/postcss.config.js
@@ -1,10 +1,16 @@
-const { join } = require('path')
-
+// Configuration for TailwindCSS v2.2.16
 module.exports = {
-  plugins: {
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
-    autoprefixer: {},
-  },
+  plugins: [require('tailwindcss'), require('autoprefixer')],
 }
+
+// Configuration for TailwindCSS v3
+// const { join } = require('path')
+
+// module.exports = {
+//   plugins: {s
+//     tailwindcss: {
+//       config: join(__dirname, 'tailwind.config.js'),
+//     },
+//     autoprefixer: {},
+//   },
+// }
