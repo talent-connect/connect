a0ceb3fbe3304a1e1e87c62c2bd9d9973b8103ca
katamatata <kathrin.voronina@gmail.com>
2024-08-15 16:20:02 +0200

    Upgrade TailwindCSS to version 3 and update the configuration

--- apps/redi-talent-pool/tailwind.config.js
+++ apps/redi-talent-pool/tailwind.config.js
@@ -1,30 +1,15 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind')
 const { join } = require('path')
 
-// Configuration for TailwindCSS v2.2.16
+// Configuration for TailwindCSS v3
 module.exports = {
-  purge: [
+  content: [
     join(__dirname, './src/**/*!(*.stories|*.spec).{js,ts,jsx,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   prefix: 'tw-',
-  darkMode: false,
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }
-
-// Configuration for TailwindCSS v3
-// module.exports = {
-//   content: [
-//     join(__dirname, './src/**/*!(*.stories|*.spec).{js,ts,jsx,tsx,html}'),
-//     ...createGlobPatternsForDependencies(__dirname),
-//   ],
-//  prefix: 'tw-',
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
