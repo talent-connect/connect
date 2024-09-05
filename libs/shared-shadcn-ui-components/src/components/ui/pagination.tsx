31a4fd649b4995f7f5fa98c793b65cd63698438f
katamatata <kathrin.voronina@gmail.com>
2024-09-05 17:53:35 +0200

    Fix a typo

--- libs/shared-shadcn-ui-components/src/components/ui/pagination.tsx
+++ libs/shared-shadcn-ui-components/src/components/ui/pagination.tsx
@@ -39,7 +39,7 @@ const PaginationItem = React.forwardRef<
   HTMLLIElement,
   React.ComponentProps<'li'>
 >(({ className, ...props }, ref) => (
-  <li ref={ref} className={classNames('tw-', className)} {...props} />
+  <li ref={ref} className={classNames('', className)} {...props} />
 ))
 PaginationItem.displayName = 'PaginationItem'
 
